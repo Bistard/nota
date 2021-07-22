138e926bbf5df5811c0733cf42826c09f5ff9502
Bistard <1015813038@qq.com>
2021-07-22 14:41:28 +0800

    💛add preload.js

--- src/main.js
+++ src/main.js
@@ -1,63 +1,74 @@
 const OS = require('os');
+const path = require('path');
 const Electron = require('electron');
 const { default : Vditor } = require('vditor');
 
 const Notification = require('./js/notification');
+const leftOutline = require('./js/leftOutline');
 
-function createWindow() {
-    const WINDOW = new Electron.BrowserWindow({
-        height: 800,
-        width: 1200,
-        minHeight: 400,
+function createMainApp(width, height) {
+    const winMain = new Electron.BrowserWindow({
+        width: width,
+        height: height,
         minWidth: 600,
-
+        minHeight: 400,
+        
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
         },
 
         show : false, 
         frame: false
     });
+
+    const view = new Electron.BrowserView();
+    winMain.setBrowserView(view);
+    view.setBounds( {x:0, y:0, width:300, height:300 } );
+    view.webContents.loadURL('https://electronjs.org');
     
-    WINDOW.loadFile('./src/index.html');
-    WINDOW.show();
+    winMain.loadFile('./src/index.html');
+    winMain.show();
 
     Electron.ipcMain.on('minApp', () => {
-        WINDOW.minimize();
+        winMain.minimize();
     })
 
     Electron.ipcMain.on('maxApp', () => {
-        if (WINDOW.isMaximized()) {
-            WINDOW.restore();
+        if (winMain.isMaximized()) {
+            winMain.restore();
         } else {
-            WINDOW.maximize();
+            winMain.maximize();
         }
     })
     
     Electron.ipcMain.on('closeApp', () => {
-        WINDOW.close();
+        winMain.close();
     })
     
+    return winMain;
 }
 
 Electron.app.whenReady().then(() => {
 
-    createWindow();
+    const winMain = createMainApp(1200, 800);
 
-
+    Electron.app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (Electron.BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    })
 
 })
 
-/* 
-Electron.app.on('window-all-closed', () => {
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+Electron.app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
-        app.quit();
+        Electron.app.quit();
     }
 })
-
-Electron.app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-}) */
