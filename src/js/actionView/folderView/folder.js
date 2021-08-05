1e73cc99a4cb066d6e21ed1ed390d497ac75b050
Bistard <1015813038@qq.com>
2021-08-05 08:27:40 +0800

    🖤fix file path not found issue

--- src/js/actionView/folderView/folder.js
+++ src/js/actionView/folderView/folder.js
@@ -3,7 +3,7 @@ const { ipcRenderer } = require('electron')
 const {readFile, writeFile } = require('fs')
 
 const folderView = document.getElementById('action-view')
-const treeContainer = document.getElementById('action-view-container')
+const treeContainer = document.getElementById('action-view-content')
 const emptyFolderTag = document.getElementById('emptyFolderTag')
 const contentView = document.getElementById('content-view')
 const resize = document.getElementById("resize")
