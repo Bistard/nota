a3caf49f063bb52979b10c36e421bcc286e81528
Jody Zhou <Jody625326@gmail.com>
2021-08-29 15:32:51 -0400

    updated check mark display logic

--- src/base/browser/secondary/contextMenu/actionBarContextMenu.ts
+++ src/base/browser/secondary/contextMenu/actionBarContextMenu.ts
@@ -7,8 +7,8 @@ export class ActionBarContextMenu extends ContextMenu {
             ContextMenuType.actionBar, 
             dimension,
             [
-                {id: 'select-explorer-button', classes: ['menu-item'], text: 'Explorer', role: 'normal'},
-                {id: 'select-outline-button', classes: ['menu-item'], text: 'Outline', role: 'normal'},
+                {id: 'select-explorer-button', classes: ['menu-item'], text: 'File Explorer', role: 'checkBox'},
+                {id: 'select-outline-button', classes: ['menu-item'], text: 'Outline', role: 'checkBox'},
                 {id: 'select-search-button', classes: ['menu-item'], text: 'Search', role: 'normal'},
                 {id: 'select-git-button', classes: ['menu-item'], text: 'Git', role: 'normal'},
             ],
@@ -18,13 +18,15 @@ export class ActionBarContextMenu extends ContextMenu {
     protected override _registerListeners(): void {
         document.getElementById('select-explorer-button')!.addEventListener('click', (ev) => {
             //ev.preventDefault();
-            console.log('good');
             const actionButton = document.getElementById("explorer-button");
+            const actionButtonContextMenu = document.getElementById("select-explorer-button-check-mark");
             console.log(actionButton?.style.display);
             if (actionButton!.style.display == 'none') {
                 actionButton!.style.display = 'initial';
+                actionButtonContextMenu!.style.filter = 'none';
             } else {
                 actionButton!.style.display = 'none';
+                actionButtonContextMenu!.style.filter = 'invert(88%) sepia(73%) saturate(4498%) hue-rotate(184deg) brightness(128%) contrast(93%)';
             } 
         })
     } 
