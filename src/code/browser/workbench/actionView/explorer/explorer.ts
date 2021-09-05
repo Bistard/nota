b3dd68c3d345557d7b0525e88ce4f2a2c071ad98
Jody Zhou <Jody625326@gmail.com>
2021-09-05 12:59:35 -0400

    fix indentation

--- src/code/browser/workbench/actionView/explorer/explorer.ts
+++ src/code/browser/workbench/actionView/explorer/explorer.ts
@@ -60,21 +60,21 @@ export class ExplorerViewComponent extends Component implements IExplorerViewSer
          * on https://www.electronjs.org/docs/api/remote
          */
 
-    /**
-                 * @readonly register context menu listeners (right click menu)
-                 */
-     document.getElementById('explorer-container')!.addEventListener('contextmenu', (ev: MouseEvent) => {
-        ev.preventDefault();
-        this.contextMenuService.removeContextMenu();
-        let coordinate: Coordinate = {
-            coordinateX: ev.pageX,
-            coordinateY: ev.pageY,
-        };
+        /**
+         * @readonly register context menu listeners (right click menu)
+        */
+        document.getElementById('explorer-container')!.addEventListener('contextmenu', (ev: MouseEvent) => {
+            ev.preventDefault();
+            this.contextMenuService.removeContextMenu();
+            let coordinate: Coordinate = {
+                coordinateX: ev.pageX,
+                coordinateY: ev.pageY,
+            };
 
-        this.contextMenuService.createContextMenu(ContextMenuType.actionView, coordinate);
+            this.contextMenuService.createContextMenu(ContextMenuType.actionView, coordinate);
 
-    });
-/*
+        });
+        /*
 
          domNodeByIdAddListener('action-view-content', 'contextmenu', (event) => {
             event.preventDefault()
