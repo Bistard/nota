132d59abcd9506ee2dfd5065d7c89da9895060d4
Jody Zhou <Jody625326@gmail.com>
2021-09-11 00:36:54 -0400

    implemented event class

--- src/base/common/event.ts
+++ src/base/common/event.ts
@@ -1,3 +1,5 @@
+import { listeners } from "process";
+import { LinkedList } from "./linkedList";
 
 export interface IEventEmitter {
     /**
@@ -50,5 +52,95 @@ export class EventEmitter implements IEventEmitter {
     }
 
 }
+
+export interface Event<T> {
+	(listener: (e: T) => any, thisArgs?: any),
+}
+
+export type Listener<T> = [(e: T) => void, any] | ((e: T) => void);
+
+export interface EmitterOptions {
+	onFirstListenerAdd?: Function;
+	onFirstListenerDidAdd?: Function;
+	onListenerDidAdd?: Function;
+	onLastListenerRemove?: Function;
+	leakWarningThreshold?: number;
+
+	/** ONLY enable this during development */
+	_profName?: string
+}
+
+export class Emitter<T> {
+    private readonly _options?: EmitterOptions
+    private _event?: Event<T>
+    private _deliveryQueue?: LinkedList<[Listener<T>, T]>;
+	protected _listeners?: LinkedList<Listener<T>>;
+
+    constructor(options?: EmitterOptions){
+        this._options = options;
+    }
+
+    get event(): Event<T> {
+        if (!this._event) {
+            this._event = (listener: (e: T) => any, thisArgs?: any) => {
+                if (!this._listeners) {
+					this._listeners = new LinkedList();
+				};
+
+                const initialListener = this._listeners.isEmpty();
+
+                if (initialListener && this._options && this._options.onFirstListenerAdd) {
+					this._options.onFirstListenerAdd(this);
+				}
+
+                const remove = this._listeners.push(!thisArgs ? listener : [listener, thisArgs]);
+
+				if (initialListener && this._options && this._options.onFirstListenerDidAdd) {
+					this._options.onFirstListenerDidAdd(this);
+				}
+
+				if (this._options && this._options.onListenerDidAdd) {
+					this._options.onListenerDidAdd(this, listener, thisArgs);
+				}
+
+            }
+        }
+
+
+        return this._event
+    }
+
+    fire(event: T): void {
+		if (this._listeners) {
+			// put all [listener,event]-pairs into delivery queue
+			// then emit all event. an inner/nested event might be
+			// the driver of this
+
+			if (!this._deliveryQueue) {
+				this._deliveryQueue = new LinkedList();
+			}
+
+			for (let listener of this._listeners) {
+				this._deliveryQueue.push([listener, event]);
+			}
+
+			while (this._deliveryQueue.size > 0) {
+				const [listener, event] = this._deliveryQueue.shift()!;
+				try {
+					if (typeof listener === 'function') {
+						listener.call(undefined, event);
+					} else {
+						listener[0].call(listener[1], event);
+					}
+				} catch (e) {
+					console.error(e);
+				}
+			}
+		}
+	}
+}
+
+
+
 
 export const EVENT_EMITTER = new EventEmitter();
