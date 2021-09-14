356afe11134aa02905e6acc387b5d2e0ed05c3aa
SIHAN LI <1015813038@qq.com>
2021-09-14 11:40:44 +0800

    improve UnitTestService

--- src/code/common/service/unitTestService.ts
+++ src/code/common/service/unitTestService.ts
@@ -1,57 +1,32 @@
+import { GlobalConfigService, IGlobalConfigService } from "src/code/common/service/configService/globalConfigService";
 import { createDecorator } from "src/code/common/service/instantiationService/decorator";
-
-// #region
-import "src/base/common/file/test/uri.test";
-// #endregion
 
 export const IUnitTestService = createDecorator<IUnitTestService>('test-service');
 
-const _staticModules: string[] = [
-    "src/base/common/file/test/uri.test.ts",
-];
-
 export interface IUnitTestService {
-    readonly modules: string[];
-    
     importModules(newModules?: string[]): void;
 }
 
 export class UnitTestService implements IUnitTestService {
 
-    public readonly modules: string[] = [];
-    private _didImport: boolean;
+    private _didImport: boolean = false;
 
-    constructor(modulesWaitToImport: string[] = [], autoImport: boolean = true) {
-        if (modulesWaitToImport) {
-            this.modules = modulesWaitToImport;
-        }
-        if (autoImport === true) {
-            this.importModules();
-            this._didImport = true;
-        } else {
-            this._didImport = false;
-        }
-    }
+    constructor(
+        @IGlobalConfigService private readonly globalConfigService: GlobalConfigService,
+    ) {
 
-    public importModules(newModules: string[] = []): void {
-        for (const module of newModules) {
-            // import(module);
-            this.modules.push(module);
+        if (this.globalConfigService.appMode === 'debug') {
+            import("src/base/common/file/test/uri.test");
+            import("src/code/common/service/test/fileService.test");
         }
         
-        if (this._didImport === true) {
-            return;
-        }
+    }
 
-        for (const module of this.modules) {
-            // import(module);
+    // DEBUG: does not work for now, you need to hardcode the import as above
+    public importModules(newModules: string[] = []): void {
+        for (const module of newModules) {
+            import(module);
         }
-
-        for (const module of _staticModules) {
-            // import(module);
-        }
-
-        this._didImport = true;
     }
 
 }
