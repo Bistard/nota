08c8e0d3bad46d07dfed2db7b3d2758b55184cc0
Jody Zhou <jody625326@gmail.com>
2021-12-28 13:32:30 -0500

    new version of event emitter

--- src/base/common/event.ts
+++ src/base/common/event.ts
@@ -1,4 +1,6 @@
 import { LinkedList } from "./linkedList";
+import { Disposable, DisposableManager, IDisposable, toDisposable } from "./dispose";
+import { listeners } from "process";
 
 export interface IEventEmitter {
     /**
@@ -53,46 +55,42 @@ export class EventEmitter implements IEventEmitter {
 }
 
 export interface Event<T> {
-	(listener: (e: T) => any, thisArgs?: any),
+	(listener: (e: T) => any, disposables?: IDisposable[]): IDisposable;
 }
 
 export type Listener<T> = [(e: T) => void, any] | ((e: T) => void);
 
-export interface EmitterOptions {
-	onFirstListenerAdd?: Function;
-	onFirstListenerDidAdd?: Function;
-	onListenerDidAdd?: Function;
-	onLastListenerRemove?: Function;
-}
 
 export class Emitter<T> {
-    private readonly _options?: EmitterOptions
+    private _disposed: boolean = false;
     private _event?: Event<T>
-    private _deliveryQueue?: LinkedList<[Listener<T>, T]>;
 	protected _listeners?: LinkedList<Listener<T>>;
-
-    constructor(options?: EmitterOptions){
-        this._options = options;
-    }
 
     get event(): Event<T> {
         if (!this._event) {
-            this._event = (listener: (e: T) => any, thisArgs?: any) => {
-                if (!this._listeners) {
+			this._event = (listener: (e: T) => any, disposables?: IDisposable[]) => {
+				if (!this._listeners) {
 					this._listeners = new LinkedList();
-				};
-
-                this._listeners.push(!thisArgs ? listener : [listener, thisArgs]);
-
-				if (this._options && this._options.onListenerDidAdd) {
-					this._options.onListenerDidAdd(this, listener, thisArgs);
 				}
 
-            }
-        }
+				const remove = this._listeners.push(listener);
 
 
-        return this._event
+				const result = toDisposable(() => {
+					if (!this._disposed) {
+						remove();
+					}
+				});
+
+				if (Array.isArray(disposables)) {
+					disposables.push(result);
+				}
+				
+
+				return result;
+			};
+		}
+		return this._event;
     }
 
     fire(event: T): void {
@@ -100,17 +98,7 @@ export class Emitter<T> {
 			// put all [listener,event]-pairs into delivery queue
 			// then emit all event. an inner/nested event might be
 			// the driver of this
-
-			if (!this._deliveryQueue) {
-				this._deliveryQueue = new LinkedList();
-			}
-
-			for (let listener of this._listeners) {
-				this._deliveryQueue.push([listener, event]);
-			}
-
-			while (this._deliveryQueue.size > 0) {
-				const [listener, event] = this._deliveryQueue.shift()!;
+            for (let listener of this._listeners) {
 				try {
 					if (typeof listener === 'function') {
 						listener.call(undefined, event);
@@ -121,6 +109,13 @@ export class Emitter<T> {
 					console.error(e);
 				}
 			}
+		}
+	}
+
+    dispose() {
+		if (!this._disposed) {
+			this._disposed = true;
+			this._listeners?.clear();
 		}
 	}
 }
