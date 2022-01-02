30b3f8180a6b014c78758900bf1720295257c5f5
SIHAN LI <1015813038@qq.com>
2022-01-02 01:45:42 -0500

    [Doc] for `Widget`

--- src/base/browser/basic/widget.ts
+++ src/base/browser/basic/widget.ts
@@ -2,18 +2,22 @@ import { Disposable } from "src/base/common/dispose";
 import { addDisposableListener, EventType } from "src/base/common/domNode";
 
 /**
- * @description // TODO
+ * @description Gives easy abilities to listen to the provided element specific 
+ * event type.
  */
 export class Widget extends Disposable {
     
+    /* Registers a callback function when the provided element is clicked */
     public onClick(element: HTMLElement, callback: (event: any) => void): void {
         this.__register(addDisposableListener(element, EventType.click, callback));
     }
 
+    /* Registers a callback function when the provided element is mouseovered */
     public onMouseover(element: HTMLElement, callback: (event: any) => void): void {
         this.__register(addDisposableListener(element, EventType.mouseover, callback));
     }
 
+    /* Registers a callback function when the provided element is mouseouted */
     public onMouseout(element: HTMLElement, callback: (event: any) => void): void {
         this.__register(addDisposableListener(element, EventType.mouseout, callback));
     }
