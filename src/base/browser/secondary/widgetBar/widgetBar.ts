de0979b8a92c69a951db2cbe9b91383fd08e0730
SIHAN LI <1015813038@qq.com>
2022-01-03 15:41:44 -0500

    [New] new tool `WidgetBar` to contain a list of `Widget`

---
+++ src/base/browser/secondary/widgetBar/widgetBar.ts
@@ -0,0 +1,157 @@
+import { IWidget, Widget } from "src/base/browser/basic/widget";
+import { Disposable, disposeAll } from "src/base/common/dispose";
+import { addDisposableListener, clearChildrenNodes, EventType } from "src/base/common/domNode";
+
+export interface IWidgetBarItem extends IWidget {
+    // right now, is equivalent with a IWidget.
+}
+
+export interface IWidgetBar extends IWidget {
+    // TODO
+}
+
+export enum WidgetBarOrientation {
+    Horizontal,
+    Vertical
+}
+
+export interface IWidgetBarOptions {
+    orientation: WidgetBarOrientation
+}
+
+/**
+ * // TODO rename to WidgetBar
+ */
+export class WidgetBar<T extends IWidget> extends Disposable {
+
+    protected readonly _container: HTMLElement;
+    protected readonly _itemsContainer: HTMLElement;
+    public items: T[];
+
+    protected opts: IWidgetBarOptions;
+
+    constructor(
+        parentContainer: HTMLElement,
+        opts: IWidgetBarOptions
+    ) {
+        super();
+
+        this.items = [];
+        this.opts = opts;
+
+        this._container = document.createElement('div');
+        this._container.classList.add('composite-bar');
+
+        this._itemsContainer = document.createElement('ui');
+        this._itemsContainer.classList.add('composite-list');
+
+        if (this.opts.orientation === WidgetBarOrientation.Vertical) {
+            this._itemsContainer.style.display = 'block';
+        }
+
+        this._container.appendChild(this._itemsContainer);
+        parentContainer.appendChild(this._container);
+    }
+
+    get container(): HTMLElement {
+        return this._container;
+    }
+
+    get viewsContainer(): HTMLElement {
+        return this._itemsContainer;
+    }
+
+    public override dispose(): void {
+        disposeAll(this.items);
+        this.items = [];
+        this._container.remove();
+        super.dispose();
+    }
+
+    public addItem(item: T, index?: number): void {
+
+        // create a new view HTMLElement
+        const newViewElement = document.createElement('li');
+        newViewElement.classList.add('composite-item');
+
+        // render the viewItem
+        item.render(newViewElement);
+
+        // prevent native context menu on the viewElement
+        this.__register(addDisposableListener(newViewElement, EventType.contextmenu, (e) => {
+            e.preventDefault();
+        }))
+
+        if (index === undefined || index < 0 || index >= this._itemsContainer.children.length) {
+            // index is not valid to be inserted, we insert at the end.
+            this._itemsContainer.appendChild(newViewElement);
+            this.items.push(item);
+        } else {
+            // index is valid.
+            this._itemsContainer.insertBefore(newViewElement, this._itemsContainer.children[index]!);
+            this.items.splice(index, 0, item);
+        }
+
+    }
+
+    public removeItem(index: number): boolean {
+        
+        // index is invalid
+        if (index < 0 || index >= this._itemsContainer.children.length) {
+            return false;
+        }
+
+        // remove the viewElement and the corresponding view.
+        this._itemsContainer.removeChild(this._itemsContainer.childNodes[index]!);
+        disposeAll(this.items.splice(index, 1));
+
+        return true;
+    }
+
+    // public getItem(id: string): T {
+    //     // TODO use this.items.filter
+    // }
+
+    // public getItemIndex(id: string): number {
+
+    // }
+
+    // public hideItems(): void {
+
+    // }
+
+    public clearItems(): number {
+        disposeAll(this.items);
+        this.items = [];
+        return clearChildrenNodes(this._itemsContainer);
+    }
+
+    public size(): number {
+        return this.items.length;
+    }
+
+    public empty(): boolean {
+        return this.items.length === 0;
+    }
+
+    public getItemWidth(index: number): number {
+        // index is invalid
+        if (index < 0 || index >= this._itemsContainer.children.length) {
+            return -1;
+        }
+
+        const viewElement = this._itemsContainer.children[index]!;
+        return viewElement.clientWidth;
+    }
+
+    public getItemHeight(index: number): number {
+        // index is invalid
+        if (index < 0 || index >= this._itemsContainer.children.length) {
+            return -1;
+        }
+
+        const viewElement = this._itemsContainer.children[index]!;
+        return viewElement.clientHeight;
+    }
+
+}
