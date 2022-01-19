83d155ffb4c3053d3f0dd5fc8ac28d4baae63794
SIHAN LI <1015813038@qq.com>
2022-01-18 20:10:46 -0500

    [Add] `DOMSize` namespace

--- src/base/common/dom.ts
+++ src/base/common/dom.ts
@@ -105,7 +105,115 @@ export function clearChildrenNodes(node: HTMLElement): number {
 	return cnt;
 }
 
+/**
+ * @description Generates a string representation for HTML <span> label to display 
+ * text.
+ * @param text The text to be displeyed.
+ * @returns The string representation of a <span> label.
+ */
 export function formatSpan(text: string): string {
 	return `<span>${text}</span>`;
 }
 
+/**
+ * @description A uitility namespace that contains all the helper functions to 
+ * get the size-related attributes from a DOM element.
+ * 
+ * @warn The namespace does NOT work for IE8 browser, see 
+ * 	https://stackoverflow.com/questions/5227909/how-to-get-an-elements-padding-value-using-javascript AND
+ * 	https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+ */
+export namespace DOMSize
+{
+
+	/**
+	 * A template function to get the resired CSS property from a given element.
+	 * @param element The HTMLElement.
+	 * @param property The CSS properpty name.
+	 * @returns The numerated resired property.
+	 * 
+	 * @warn If property cannot be convert to numerated form, -1 will be returned.
+	 */
+	function __getPropertyValue(element: HTMLElement, property: string): number {
+		let computedStyle: CSSStyleDeclaration = getComputedStyle(element);
+		let value = computedStyle.getPropertyValue(property);
+		return parseFloat(value) || -1;
+	}
+
+	// [method - padding]
+
+	export function getPaddingTop(element: HTMLElement): number {
+		return __getPropertyValue(element, 'padding-top');
+	}
+
+	export function getPaddingBottom(element: HTMLElement): number {
+		return __getPropertyValue(element, 'padding-bottom');
+	}
+
+	export function getPaddingLeft(element: HTMLElement): number {
+		return __getPropertyValue(element, 'padding-left');
+	}
+
+	export function getPaddingRight(element: HTMLElement): number {
+		return __getPropertyValue(element, 'padding-right');
+	}
+
+	// [method - border]
+
+	export function getBorderTop(element: HTMLElement): number {
+		return __getPropertyValue(element, 'border-top-width');
+	}
+
+	export function getBorderBottom(element: HTMLElement): number {
+		return __getPropertyValue(element, 'border-bottom-width');
+	}
+
+	export function getBorderLeft(element: HTMLElement): number {
+		return __getPropertyValue(element, 'border-left-width');
+	}
+
+	export function getBorderRight(element: HTMLElement): number {
+		return __getPropertyValue(element, 'border-right-width');
+	}
+
+	// [method - margin]
+
+	export function getMarginTop(element: HTMLElement): number {
+		return __getPropertyValue(element, 'margin-top');
+	}
+
+	export function getMarginBottom(element: HTMLElement): number {
+		return __getPropertyValue(element, 'margin-bottom');
+	}
+
+	export function getMarginLeft(element: HTMLElement): number {
+		return __getPropertyValue(element, 'margin-left');
+	}
+
+	export function getMarginRight(element: HTMLElement): number {
+		return __getPropertyValue(element, 'margin-right');
+	}
+
+	// [method - content]
+
+	/**
+	 * @description Get the height of the content excluding padding and border.
+	 * @param element The HTMLElement.
+	 */
+	export function getContentHeight(element: HTMLElement): number {
+		const padding = getPaddingTop(element) + getPaddingBottom(element);
+		const border  = getBorderTop(element) + getBorderBottom(element);
+		return element.offsetHeight - padding - border;
+	}
+
+	/**
+	 * @description Get the width of the content excluding padding and border.
+	 * @param element The HTMLElement.
+	 */
+	 export function getContentWidth(element: HTMLElement): number {
+		const padding = getPaddingLeft(element) + getPaddingRight(element);
+		const border  = getBorderLeft(element) + getBorderRight(element);
+		return element.offsetWidth - padding - border;
+	}
+
+}
