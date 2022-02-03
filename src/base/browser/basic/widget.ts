631b10b0c155781d06271af702a2f7b73b930255
SIHAN LI <1015813038@qq.com>
2022-02-03 15:46:26 -0500

    [Doc] comment

--- src/base/browser/basic/widget.ts
+++ src/base/browser/basic/widget.ts
@@ -81,6 +81,7 @@ export abstract class Widget extends Disposable implements IWidget {
         }));
     }
 
+    /* Registers a callback function when the provided element is on-wheel scrolling */
     public onWheel(element: HTMLElement, callback: (event: WheelEvent) => void): IDisposable {
         return this.__register(addDisposableListener(element, EventType.wheel, (e: WheelEvent) => {
             callback(e);
