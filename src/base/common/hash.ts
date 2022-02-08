7c898eb0aaae918079783f9667e8785e87bf562e
SIHAN LI <1015813038@qq.com>
2022-02-08 12:32:56 -0500

    [New] `hash.ts`

---
+++ src/base/common/hash.ts
@@ -0,0 +1,13 @@
+
+/**
+ * @description Hashes a given string into number.
+ * @param s The given string.
+ * @returns The number hash code. e.g. '1481881795', '7441251790', etc...
+ */
+export function hash(s: string): number {
+    var h = 0, i = s.length;
+    while (i > 0) {
+        h = (h << 5) - h + s.charCodeAt(--i) | 0;
+    }
+    return h; 
+};
