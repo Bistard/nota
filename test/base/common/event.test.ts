cafc71efce40a4324d1fa70b56854fdf1c1b8c40
SIHAN LI <1015813038@qq.com>
2022-02-11 15:01:51 -0500

    [Add] more unit tests on `Event`

--- test/base/common/event.test.ts
+++ test/base/common/event.test.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert';
 import { IDisposable } from 'src/base/common/dispose';
-import { Emitter } from 'src/base/common/event';
+import { DelayableEmitter, Emitter, Event, PauseableEmitter, SignalEmitter } from 'src/base/common/event';
 
 suite('event-test', () => {
 
@@ -140,6 +140,138 @@ suite('event-test', () => {
         assert.strictEqual(errors.length, 1);
         assert.strictEqual((errors[0] as Error).message, 'expect error');
         assert.strictEqual(counter, 2);
+    });
+
+    test('PauseableEmitter - basic', () => {
+        
+        const emitter = new PauseableEmitter<void>();
+
+        let cnt = 0;
+        const listener = () => { cnt++ };
+        emitter.registerListener(listener);
+
+        emitter.fire();
+        assert.strictEqual(cnt, 1);
+
+        emitter.pause();
+        emitter.fire();
+        assert.strictEqual(cnt, 1);
+        emitter.fire();
+        assert.strictEqual(cnt, 1);
+
+        emitter.resume();
+        emitter.fire();
+        assert.strictEqual(cnt, 2);
+        emitter.fire();
+        assert.strictEqual(cnt, 3);
+
+        emitter.dispose();
+        emitter.fire();
+        assert.strictEqual(cnt, 3);
+    });
+
+    test('DelayableEmitter - basic', () => {
+        const emitter = new DelayableEmitter<number>();
+
+        let cnt = 0;
+        const listener = (e: number) => cnt++;;
+        emitter.registerListener(listener);
+
+        emitter.fire(cnt);
+        assert.strictEqual(cnt, 1);
+
+        emitter.pause();
+        emitter.fire(cnt);
+        assert.strictEqual(cnt, 1);
+        emitter.fire(cnt);
+        assert.strictEqual(cnt, 1);
+
+        emitter.resume();
+        assert.strictEqual(cnt, 3);
+        emitter.fire(cnt);
+        assert.strictEqual(cnt, 4);
+
+        emitter.dispose();
+        emitter.fire(cnt);
+        assert.strictEqual(cnt, 4);
+    });
+
+    test('SingalEmitter - basic', () => {
+        const consumed1 = new Emitter<string>();
+        const consumed2 = new Emitter<string>();
+        
+        const emitter = new SignalEmitter<string, boolean>([consumed1.registerListener, consumed2.registerListener], (str) => {
+            if (str.length > 4) return true;
+            else return false;
+        });
+
+        let result = false;
+        emitter.registerListener((bool: boolean) => {
+            result = bool;
+        });
+
+        consumed1.fire('abc');
+        assert.strictEqual(result, false);
+        consumed1.fire('abcd');
+        assert.strictEqual(result, false);
+        consumed1.fire('abcde');
+        assert.strictEqual(result, true);
+        
+        consumed2.fire('abc');
+        assert.strictEqual(result, false);
+        consumed2.fire('abcde');
+        assert.strictEqual(result, true);
+
+        emitter.dispose();
+
+        result = false;
+        consumed1.fire('abcde');
+        assert.strictEqual(result, false);
+    });
+
+    test('Event::map()', () => {
+        const emitter = new Emitter<string>();
+
+        let result = false;
+
+        Event.map<string, boolean>(emitter.registerListener, (e: string) => {
+            if (e.length > 4) return true;
+            return false;
+        })((e: boolean) => {
+            result = e;
+        });
+
+        emitter.fire('abc');
+        assert.strictEqual(result, false);
+        emitter.fire('abcde');
+        assert.strictEqual(result, true);
+
+        result = false;
+        emitter.dispose();
+        emitter.fire('abcde');
+        assert.strictEqual(result, false);
+    });
+
+    test('Event:each()', () => {
+        const emitter = new Emitter<boolean>();
+
+        let result = false;
+
+        Event.each<boolean>(emitter.registerListener, (e: boolean) => {
+            return !e;
+        })((e: boolean) => {
+            result = e;
+        });
+
+        emitter.fire(true);
+        assert.strictEqual(result, false);
+        emitter.fire(false);
+        assert.strictEqual(result, true);
+
+        result = false;
+        emitter.dispose();
+        emitter.fire(false);
+        assert.strictEqual(result, false);
     });
 
     
