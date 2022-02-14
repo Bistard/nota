14a9959c51719e7535ed26230e7a6f38d050a7e2
SIHAN LI <1015813038@qq.com>
2022-02-14 02:01:02 -0500

    [Doc] for `IpcService`

--- src/code/browser/service/ipcService.ts
+++ src/code/browser/service/ipcService.ts
@@ -1,5 +1,4 @@
-import { delayFor } from "src/base/common/async";
-import { AsyncEmitter, AsyncRegister, Register } from "src/base/common/event";
+import { AsyncEmitter, AsyncRegister } from "src/base/common/event";
 import { ipcRendererOn, ipcRendererSend } from "src/base/electron/register";
 import { createDecorator } from "src/code/common/service/instantiationService/decorator";
 
@@ -7,12 +6,16 @@ export const IIpcService = createDecorator<IIpcService>('ipc-service');
 
 export interface IIpcService {
 
+    /**
+     * Fires when the application is about to be closed.
+     */
     onApplicationClose: AsyncRegister<void>;
 
 }
 
 /**
- * @description 
+ * @description A very simple service that supports application activity-related 
+ * events.
  */
 export class IpcService implements IIpcService {
     
