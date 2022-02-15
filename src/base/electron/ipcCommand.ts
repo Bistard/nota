da280ed594ad224b50136a077e241a12bf451104
SIHAN LI <1015813038@qq.com>
2022-02-15 00:01:43 -0500

    [New] new enum `IpcCommand`

---
+++ src/base/electron/ipcCommand.ts
@@ -0,0 +1,12 @@
+
+
+/**
+ * @enum A series of enum for ipc communication. The main reason having this 
+ * enum is only for reducing long string parameter from optimization perspective.
+ */
+export const enum IpcCommand {
+
+    Test = '0',
+    OpenDevelopTool = '1',
+
+}
