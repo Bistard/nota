1cdd84baef8367df2240121b7b2c6d03921c3df8
SIHAN LI <1015813038@qq.com>
2022-05-09 01:19:55 -0400

    [New] `treeRenderer` startup

---
+++ src/base/browser/basic/tree/treeRenderer.ts
@@ -0,0 +1,10 @@
+import { IListViewRenderer } from "src/base/browser/secondary/listView/listRenderer";
+
+/**
+ * // TODO
+ */
+export interface ITreeRenderer<T> extends IListViewRenderer<T> {
+
+    
+
+}
