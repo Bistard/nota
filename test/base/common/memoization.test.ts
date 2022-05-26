b8e28105ebac5864f116599ddfc1ea9b7f0a7f02
Bistard <lshh1015813038@gmail.com>
2022-05-26 02:23:00 -0400

    [New] memoization

---
+++ test/base/common/memoization.test.ts
@@ -0,0 +1,96 @@
+import * as assert from 'assert';
+import { memoize } from 'src/base/common/memoization';
+
+suite('memoization', () => {
+
+    test('memoize - class method', () => {
+        class Foo {
+			public count = 0;
+
+			constructor(private _answer: number | null | undefined) { }
+			
+            @memoize 
+            public answer(): number | null | undefined {
+				this.count++;
+				return this._answer;
+			}
+		}
+
+		const foo = new Foo(42);
+		assert.strictEqual(foo.count, 0);
+		assert.strictEqual(foo.answer(), 42);
+		assert.strictEqual(foo.count, 1);
+		assert.strictEqual(foo.answer(), 42);
+		assert.strictEqual(foo.count, 1);
+
+		const foo2 = new Foo(1337);
+		assert.strictEqual(foo2.count, 0);
+		assert.strictEqual(foo2.answer(), 1337);
+		assert.strictEqual(foo2.count, 1);
+		assert.strictEqual(foo2.answer(), 1337);
+		assert.strictEqual(foo2.count, 1);
+
+		assert.strictEqual(foo.answer(), 42);
+		assert.strictEqual(foo.count, 1);
+
+		const foo3 = new Foo(null);
+		assert.strictEqual(foo3.count, 0);
+		assert.strictEqual(foo3.answer(), null);
+		assert.strictEqual(foo3.count, 1);
+		assert.strictEqual(foo3.answer(), null);
+		assert.strictEqual(foo3.count, 1);
+
+		const foo4 = new Foo(undefined);
+		assert.strictEqual(foo4.count, 0);
+		assert.strictEqual(foo4.answer(), undefined);
+		assert.strictEqual(foo4.count, 1);
+		assert.strictEqual(foo4.answer(), undefined);
+		assert.strictEqual(foo4.count, 1);
+    });
+
+    test('memoize - class getter', () => {
+        class Foo {
+			public count = 0;
+
+			constructor(private _answer: number | null | undefined) { }
+
+			@memoize
+			get answer(): number | null | undefined {
+				this.count++;
+				return this._answer;
+			}
+		}
+
+		const foo = new Foo(42);
+		assert.strictEqual(foo.count, 0);
+		assert.strictEqual(foo.answer, 42);
+		assert.strictEqual(foo.count, 1);
+		assert.strictEqual(foo.answer, 42);
+		assert.strictEqual(foo.count, 1);
+
+		const foo2 = new Foo(1337);
+		assert.strictEqual(foo2.count, 0);
+		assert.strictEqual(foo2.answer, 1337);
+		assert.strictEqual(foo2.count, 1);
+		assert.strictEqual(foo2.answer, 1337);
+		assert.strictEqual(foo2.count, 1);
+
+		assert.strictEqual(foo.answer, 42);
+		assert.strictEqual(foo.count, 1);
+
+		const foo3 = new Foo(null);
+		assert.strictEqual(foo3.count, 0);
+		assert.strictEqual(foo3.answer, null);
+		assert.strictEqual(foo3.count, 1);
+		assert.strictEqual(foo3.answer, null);
+		assert.strictEqual(foo3.count, 1);
+
+		const foo4 = new Foo(undefined);
+		assert.strictEqual(foo4.count, 0);
+		assert.strictEqual(foo4.answer, undefined);
+		assert.strictEqual(foo4.count, 1);
+		assert.strictEqual(foo4.answer, undefined);
+		assert.strictEqual(foo4.count, 1);
+    });
+
+});
