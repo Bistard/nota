72ef0c8f47c87112e3b9b6606decf73086c42931
Bistard <lshh1015813038@gmail.com>
2022-06-03 00:00:04 -0400

    [Add] `TextBufferBuilder` unit test

---
+++ test/editor/model/textBuffer.text.ts
@@ -0,0 +1,74 @@
+import * as assert from 'assert';
+import { TextBuffer, TextBufferBuilder } from 'src/editor/model/textBuffer';
+
+
+suite('textBufferBuilder', () => {
+
+    test('basic build', () => {
+
+        const builder = new TextBufferBuilder();
+        builder.receive('chris');
+        builder.receive('fiona\n');
+        builder.receive('peter\r\n');
+
+        builder.build();
+
+        const chunks = (builder as any)._chunks as TextBuffer[];
+        assert.strictEqual(chunks[0]!.buffer, 'chris');
+        assert.strictEqual(chunks[0]!.linestart, []);
+        assert.strictEqual(chunks[1]!.buffer, 'fiona\n');
+        assert.strictEqual(chunks[1]!.linestart, [6]);
+        assert.strictEqual(chunks[2]!.buffer, 'peter\r\n');
+        assert.strictEqual(chunks[2]!.linestart, [7]);
+    });
+
+    test('empty build', () => {
+        const builder = new TextBufferBuilder();
+
+        builder.build();
+
+        const chunks = (builder as any)._chunks as TextBuffer[];
+        assert.strictEqual(chunks[0]!.buffer, '');
+        assert.strictEqual(chunks[0]!.linestart, []);
+    });
+
+    test('oneline build', () => {
+        const builder = new TextBufferBuilder();
+
+        builder.receive('chris');
+        builder.build();
+
+        const chunks = (builder as any)._chunks as TextBuffer[];
+        assert.strictEqual(chunks[0]!.buffer, 'chris');
+        assert.strictEqual(chunks[0]!.linestart, []);
+    });
+
+    test('chunk ended with carriage return', () => {
+        const builder = new TextBufferBuilder();
+        builder.receive('fiona\r');
+        builder.receive('\npeter\r\n');
+
+        builder.build();
+
+        const chunks = (builder as any)._chunks as TextBuffer[];
+        assert.strictEqual(chunks[0]!.buffer, 'fiona');
+        assert.strictEqual(chunks[0]!.linestart, []);
+        assert.strictEqual(chunks[1]!.buffer, '\r\npeter\r\n');
+        assert.strictEqual(chunks[1]!.linestart, [2, 9]);
+    });
+
+    test('chunk ended with surrogates', () => {
+        const builder = new TextBufferBuilder();
+        builder.receive('fiona😁');
+        builder.receive('peter\r\n');
+
+        builder.build();
+
+        const chunks = (builder as any)._chunks as TextBuffer[];
+        assert.strictEqual(chunks[0]!.buffer, 'fiona');
+        assert.strictEqual(chunks[0]!.linestart, []);
+        assert.strictEqual(chunks[1]!.buffer, '😁peter\r\n');
+        assert.strictEqual(chunks[1]!.linestart, [9]);
+    });
+
+});
