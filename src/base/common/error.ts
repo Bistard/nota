661fa7357f62ca8b4bf248e43a7f26d6ed657e0f
Bistard <lshh1015813038@gmail.com>
2022-08-05 04:05:27 -0400

    [New] `error.ts` able to caught unexpected error

---
+++ src/base/common/error.ts
@@ -0,0 +1,118 @@
+import { IDisposable } from "src/base/common/dispose";
+
+interface IErrorCallback {
+    (error: any): void;
+}
+
+type IErrorListener = IErrorCallback;
+
+/**
+ * @class A unexposed singleton that manages to handle all the unexpect errors
+ * that caught by the {@link ErrorHandler.onUnexpectedError()}.
+ * 
+ * {@link _ErrorRegistry} cannot be access directly. All other functionalites can 
+ * be found in {@link ErrorHandler}.
+ * 
+ * @note Default onUnexpectedError behaviour is by calling `console.error(err)`
+ * then simply throw it out.
+ */
+const _ErrorRegistry = new class extends class ErrorRegistry {
+    
+    // [field]
+
+    private _unexpectedErrorExternalCallback: IErrorCallback;
+    private _listeners: IErrorListener[] = [];
+
+    // [constructor]
+
+    constructor() {
+        this._unexpectedErrorExternalCallback = (error: any) => {
+            console.error(error);
+            throw error;
+        };
+    }
+
+    // [public methods]
+
+    public setUnexpectedErrorExternalCallback(callback: IErrorCallback): void {
+        this._unexpectedErrorExternalCallback = callback;
+    }
+    
+    public onUnexpectedError(error: any): void {
+        this._unexpectedErrorExternalCallback(error);
+        this.__fire(error);
+    }
+
+    public onUnexpectedExternalError(error: any): void {
+        this._unexpectedErrorExternalCallback(error);
+    }
+
+    public registerListener(listener: IErrorListener): IDisposable {
+        this._listeners.push(listener);
+        return {
+            dispose: () => {
+                this.__removeListener(listener);
+            }
+        };
+    }
+
+    // [private helper methods]
+
+    private __fire(error: any): void {
+        for (const listener of this._listeners) {
+            listener(error);
+        }
+    }
+
+    private __removeListener(listener: IErrorListener): void {
+        this._listeners.splice(this._listeners.indexOf(listener), 1);
+    }
+
+} {};
+
+/**
+ * @namespace ErrorHandler Supports a series of functions to handle unexpected
+ * errors.
+ * 
+ * @note Since handler idealy should handling unexpected errors, thus those 
+ * errors should be very important and even be fatal to some functionalities of 
+ * the application. In that case, use these functions CAREFULLY.
+ */
+export namespace ErrorHandler {
+    
+    /**
+     * @description Register a listener on unexpected errors (will not recieve 
+     * external error fired by {@link ErrorHandler.onUnexpectedExternalError}).
+     * @returns Returns a {@link IDisposable} for unregisteration.
+     */
+    export function registerListener(listener: IErrorListener): IDisposable {
+        return _ErrorRegistry.registerListener(listener);
+    }
+
+    /**
+     * @description Set a new unexpected error handler callback when any errors 
+     * is caught (including external errors).
+     */
+    export function setUnexpectedErrorExternalCallback(callback: IErrorCallback): void {
+        _ErrorRegistry.setUnexpectedErrorExternalCallback(callback);
+    }
+
+    /**
+     * @description Fires an error that will be notified to all the listeners 
+     * and invoked by the
+     * @param error The unexpected error to be fired.
+     */
+    export function onUnexpectedError(error: any): void {
+        _ErrorRegistry.onUnexpectedError(error);
+    }
+
+    /**
+     * @description Different than {@link ErrorHandler.onUnexpectedError}, the
+     * registered listeners will not be notified by this error since this is 
+     * for external purpose.
+     */
+    export function onUnexpectedExternalError(error: any): void {
+        _ErrorRegistry.onUnexpectedExternalError(error);
+    }
+
+}
