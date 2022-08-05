8302107837235d89e3d3773d0aed55bd3f56ba47
Bistard <lshh1015813038@gmail.com>
2022-08-05 07:44:27 -0400

    [Test] remove syntax error for now

--- src/base/electron/app.ts
+++ src/base/electron/app.ts
@@ -1,25 +1,24 @@
+import { app } from "electron";
 
-// TODO: 'remote' is deprecated, use others instead
-import { remote } from "electron";
 
 /**
  * The current application directory.
  */
-export const APP_ROOT_PATH = remote.app.getAppPath();
+export const APP_ROOT_PATH = app.getAppPath();
 
 /**
  * The desktop directory.
  */
-export const DESKTOP_ROOT_PATH = remote.app.getPath('desktop');
+export const DESKTOP_ROOT_PATH = app.getPath('desktop');
 
 /**
  * The download directory.
  */
-export const DOWNLOAD_ROOT_PATH = remote.app.getPath('downloads');
+export const DOWNLOAD_ROOT_PATH = app.getPath('downloads');
 
 /**
  * A Boolean property that returns true if the app is packaged, false otherwise. 
  * For many apps, this property can be used to distinguish development and 
  * production environments.
  */
-export const DEVELOP_ENV = !remote.app.isPackaged;
+export const DEVELOP_ENV = !app.isPackaged;
