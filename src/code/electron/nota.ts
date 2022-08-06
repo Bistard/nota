847c0234f6d3c329d779561b762de5665d806b51
Bistard <lshh1015813038@gmail.com>
2022-08-06 08:11:14 -0400

    [Add] `NotaInstance` run() framework

--- src/code/electron/nota.ts
+++ src/code/electron/nota.ts
@@ -1,11 +1,18 @@
+import { app } from "electron";
 import { Disposable } from "src/base/common/dispose";
+import { ErrorHandler } from "src/base/common/error";
+import { Event } from "src/base/common/event";
 import { ILogService } from "src/base/common/logger";
 import { IGlobalConfigService, IUserConfigService } from "src/code/common/service/configService/configService";
 import { IFileService } from "src/code/common/service/fileService/fileService";
-import { IInstantiationService } from "src/code/common/service/instantiationService/instantiation";
+import { IInstantiationService, IServiceProvider } from "src/code/common/service/instantiationService/instantiation";
 import { IEnvironmentService, IMainEnvironmentService } from "src/code/platform/enviroment/common/environment";
 import { IMainLifeCycleService } from "src/code/platform/lifeCycle/electron/mainLifeCycleService";
+import { IWindowInstance } from "src/code/platform/window/common/window";
 
+/**
+ * An interface only for {@link NotaInstance}
+ */
 export interface INotaInstance {
     run(): Promise<void>;
 }
@@ -29,17 +36,63 @@ export class NotaInstance extends Disposable implements INotaInstance {
         @IUserConfigService private readonly userConfigService: IUserConfigService
     ) {
         super();
+        this.registerListeners();
     }
 
     // [public methods]
 
-    /**
-     * // TODO
-     */
     public async run(): Promise<void> {
+        this.logService.debug('nota starting...');
+        this.logService.debug(`${this.environmentService.appRootPath}`);
+
+        // application service initialization
+        const appInstantiationService = await this.registerServices();
+
+        // open first window
+        const window = this.openFirstWindow(appInstantiationService);
+
+        // post work
+        this.afterFirstWindow(appInstantiationService);
+    }
+
+    // [private methods]
+
+    private registerListeners(): void {
+
+        Event.once(this.lifeCycleService.onWillQuit)(() => this.dispose());
+
+        // interept unexpected errors so that the error will not go back to `main.ts`
+        process.on('uncaughtException', err => ErrorHandler.onUnexpectedError(err));
+		process.on('unhandledRejection', reason => ErrorHandler.onUnexpectedError(reason));
+        ErrorHandler.setUnexpectedErrorExternalCallback(err => this.__onUnexpectedError(err));
         
+        app.on('open-file', (event, path) => {
+            this.logService.trace('main#app.on("open-file"): ', path);
+            // REVIEW
+        });
+    }
+
+    private async registerServices(): Promise<IInstantiationService> {
+        // TODO
+        return (void 0) as unknown as any;
+    }
+
+    private openFirstWindow(serviceProvider: IServiceProvider): IWindowInstance {
+        // TODO
+        return (void 0) as unknown as any;
+    }
+
+    private afterFirstWindow(serviceProvider: IServiceProvider): void {
+        // TODO
     }
 
     // [private helper methods]
+
+    private __onUnexpectedError(error: any): void {
+        this.logService.error(`[uncought exception]: ${error}`);
+        if (error.stack) {
+            this.logService.error(error.stack);
+        }
+    }
 
 }
---
+++ src/code/platform/window/common/window.ts
@@ -0,0 +1,11 @@
+
+export interface IWindowInstance {
+    // TODO
+}
+
+/**
+ * An interface only for {@link MainWindowService}.
+ */
+export interface IMainWindowService {
+    // TODO
+}
