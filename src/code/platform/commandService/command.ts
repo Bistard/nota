1d34668c03e7ccf363180fc1d2a8ca6b203da6d2
Lingzhi Meng <1mengjer2@gmail.com>
2022-08-06 23:08:26 -0400

    [Add] Basic functions of #CommandRegistrant

---
+++ src/code/platform/commandService/command.ts
@@ -0,0 +1,57 @@
+import { IDisposable, toDisposable } from "src/base/common/dispose";
+import { IServiceProvider } from "src/code/common/service/instantiationService/instantiation";
+
+export interface ICommand {
+    readonly id: string;
+    readonly description?: string;
+    readonly executor: ICommandExecutor;
+}
+
+export interface ICommandExecutor {
+    (provider: IServiceProvider, ...args: any[]): void;
+} 
+
+export interface ICommandRegistrant {
+
+    // TODO
+    registerCommand(id: string, executor: ICommandExecutor, description?: string): IDisposable;
+
+    // TODO
+    getCommand(id: string): ICommand | undefined;
+
+    getAllCommands(): any;
+}
+
+// TODO
+export const CommandRegistrant: ICommandRegistrant = new class implements ICommandRegistrant {
+
+    private _commands = new Map<string, ICommand>();
+
+    constructor() {
+        // noop
+    }
+
+    public registerCommand(id: string, executor: ICommandExecutor, description?: string, overwrite: boolean = false): IDisposable {
+        if (!description) {
+            description = 'No descriptions';
+        }
+
+        if (overwrite === true || !this._commands.has(id)) {
+            this._commands.set(id, {id, executor, description});
+        }
+        
+        let unregister = toDisposable(() => {
+            this._commands.delete(id);
+        });
+        return unregister;
+    }
+
+    public getCommand(id: string): ICommand | undefined {
+        const command = this._commands.get(id);
+        return command;
+    }
+
+    public getAllCommands(): any {
+        return this._commands;
+    }
+}
