483087bcd460702cba659de50bf7b618ef8a0e2e
Bistard <lshh1015813038@gmail.com>
2022-08-10 01:07:58 -0400

    [Add] more robust preload.js

--- src/code/platform/window/electron/preload.js
+++ src/code/platform/window/electron/preload.js
@@ -26,64 +26,111 @@
 
 	const { contextBridge, ipcRenderer } = require('electron');
 
-	console.log(process.argv);
-	console.log('[preload]');
-
-	const myIpcRenderer = {
-		send(channel, ...args) {
-			ipcRenderer.send(channel, ...args);
-		},
-
-		on(channel, listener) {
-			ipcRenderer.on(channel, listener);
-			return this;
-		},
-
-		removeListener(channel, listener) {
-			ipcRenderer.removeListener(channel, listener);
-			return this;
+	// REVIEW
+	function retrieveFromAdditionalArgv(key) {
+		for (const arg of process.argv) {
+			if (arg.indexOf(`--${key}=`) === 0) {
+				// found
+				return arg.split('=')[1];
+			}
 		}
-	};
+		return undefined;
+	}
 
-	const myProcess = {
-		get platform() { return process.platform; },
-		get arch() { return process.arch; },
-		get env() { return { ...process.env }; },
-		get pid() { return process.pid; },
-		get versions() { return process.versions; },
-		get type() { return 'renderer'; },
-		get execPath() { return process.execPath; },
-		get sandboxed() { return process.sandboxed; },
-		cwd() {
-			return process.env['VSCODE_CWD'] || process.execPath.substr(0, process.execPath.lastIndexOf(process.platform === 'win32' ? '\\' : '/'));
-		},
-
-		shellEnv() {
-			return resolveShellEnv;
-		},
-
-		getProcessMemoryInfo() {
-			return process.getProcessMemoryInfo();
-		},
-
-		on(type, callback) {
-			process.on(type, callback);
+	function validate(channel) {
+		if (!channel || !channel.startsWith('nota:')) {
+			throw new Error(`Unsupported event IPC channel '${channel}'`);
 		}
-	};
-
-	const globalAPIs = {
-		ipcRenderer: myIpcRenderer,
-		process: myProcess,
-	};
+		return true;
+	}
 
 	/**
-	 * If `contextIsolation` is true, we expose the global APIs to the renderer
-	 * process. Otherwise we simply put them in the `window` variable.
+	 * You never want to directly expose the entire ipcRenderer or other similar
+	 * modules via `preload.js`. This would give your renderer the ability to 
+	 * send arbitrary IPC messages to the main process, which becomes a powerful 
+	 * attack vector for malicious code.
 	 */
+
+	const wrappedIpcRenderer = (function wrapIpcRendererAPI() {
+		return {
+			 send(channel, ...args) {
+				if (validate(channel)) {
+					ipcRenderer.send(channel, ...args);
+				}
+			},
+			invoke(channel, ...args) {
+				if (validate(channel)) {
+					return ipcRenderer.invoke(channel, ...args);
+				}
+			},
+			on(channel, listener) {
+				if (validate(channel)) {
+					ipcRenderer.on(channel, listener);
+
+					return this;
+				}
+			},
+			once(channel, listener) {
+				if (validate(channel)) {
+					ipcRenderer.once(channel, listener);
+
+					return this;
+				}
+			},
+			removeListener(channel, listener) {
+				if (validate(channel)) {
+					ipcRenderer.removeListener(channel, listener);
+
+					return this;
+				}
+			}
+		};
+	})();
+
+	const wrappedProcess = (function wrapProcessAPI() {
+		return {
+			get platform() { return process.platform; },
+			get arch() { return process.arch; },
+			get env() { return { ...process.env }; },
+			get pid() { return process.pid; },
+			get versions() { return process.versions; },
+			get type() { return 'renderer'; },
+			get execPath() { return process.execPath; },
+			get sandboxed() { return process.sandboxed; },
+			cwd() {
+				return process.env['NOTA_CWD'] || process.execPath.substr(0, process.execPath.lastIndexOf(process.platform === 'win32' ? '\\' : '/'));
+			},
+	
+			shellEnv() {
+				return resolveShellEnv;
+			},
+	
+			getProcessMemoryInfo() {
+				return process.getProcessMemoryInfo();
+			},
+	
+			on(type, callback) {
+				process.on(type, callback);
+			}
+		};
+	})();
+
+	
+	/**
+	 * If `contextIsolation` is true, we expose the global APIs to the renderer
+	 * process. Otherwise we simply put them in the `window` variable since
+	 * `window` is not isolated from the renderer process.
+	 */
+	
+	const exposedAPIs = {
+		ipcRenderer: wrappedIpcRenderer,
+		process: wrappedProcess,
+	};
+
 	if (process.contextIsolated) {
-		contextBridge.exposeInMainWorld('nota', globalAPIs);
+		contextBridge.exposeInMainWorld('nota', exposedAPIs);
 	} else {
-		window.nota = globalAPIs;
+		window.nota = exposedAPIs;
 	}
 
 })();
