806f6325a896caebb471da70e8efdb0bb0e6bcb7
SIHAN LI <1015813038@qq.com>
2022-09-01 02:30:03 -0400

    [New] new `Debouncer` class

--- test/base/common/util/async.test.ts
+++ test/base/common/util/async.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-import { AsyncRunner, Blocker, delayFor, Throttler } from 'src/base/common/util/async';
+import { AsyncRunner, Blocker, Debouncer, delayFor, MicrotaskDelay, Throttler } from 'src/base/common/util/async';
 
 suite('async-test', () => {
 
@@ -56,7 +56,7 @@ suite('async-test', () => {
     });
 
     suite('throttler', () => {
-        test('sync task', function () {
+        test('sync task', async function () {
 			let count = 0;
 			const factory = () => Promise.resolve(++count);
 
@@ -95,7 +95,7 @@ suite('async-test', () => {
 		});
 
 		test('last factory should be the one getting called', function () {
-			const factoryFactory = (n: number) => () => {
+			const factoryFactory = (n: number) => async () => {
 				return delayFor(0).then(() => n);
 			};
 
@@ -111,4 +111,205 @@ suite('async-test', () => {
 		});
     });
 
+    suite('debouncer', function () {
+
+		test('simple', async () => {
+			let count = 0;
+			const factory = () => {
+				return Promise.resolve(++count);
+			};
+
+			const delayer = new Debouncer(0);
+			const promises: Promise<any>[] = [];
+
+			assert.ok(!delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then((result) => { assert.strictEqual(result, 1); assert.ok(!delayer.onSchedule()); }));
+			assert.ok(delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then((result) => { assert.strictEqual(result, 1); assert.ok(!delayer.onSchedule()); }));
+			assert.ok(delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then((result) => { assert.strictEqual(result, 1); assert.ok(!delayer.onSchedule()); }));
+			assert.ok(delayer.onSchedule());
+
+			return Promise.all(promises).then(() => {
+				assert.ok(!delayer.onSchedule());
+			});
+		});
+
+		test('microtask delay simple', async () => {
+			let count = 0;
+			const factory = () => {
+				return Promise.resolve(++count);
+			};
+
+			const delayer = new Debouncer(MicrotaskDelay);
+			const promises: Promise<any>[] = [];
+
+			assert.ok(!delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then((result) => { assert.strictEqual(result, 1); assert.ok(!delayer.onSchedule()); }));
+			assert.ok(delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then((result) => { assert.strictEqual(result, 1); assert.ok(!delayer.onSchedule()); }));
+			assert.ok(delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then((result) => { assert.strictEqual(result, 1); assert.ok(!delayer.onSchedule()); }));
+			assert.ok(delayer.onSchedule());
+
+			return Promise.all(promises).then(() => {
+				assert.ok(!delayer.onSchedule());
+			});
+		});
+
+		test('simple cancel', function () {
+			let count = 0;
+			const factory = () => {
+				return Promise.resolve(++count);
+			};
+
+			const delayer = new Debouncer(0);
+
+			assert.ok(!delayer.onSchedule());
+
+			const p = delayer.queue(factory).then(() => {
+				assert.ok(false);
+			}, () => {
+				assert.ok(true, 'yes, it was cancelled');
+			});
+
+			assert.ok(delayer.onSchedule());
+			delayer.unschedule();
+			assert.ok(!delayer.onSchedule());
+
+			return p;
+		});
+
+		test('simple cancel microtask', function () {
+			let count = 0;
+			const factory = () => {
+				return Promise.resolve(++count);
+			};
+
+			const delayer = new Debouncer(MicrotaskDelay);
+
+			assert.ok(!delayer.onSchedule());
+
+			const p = delayer.queue(factory).then(() => {
+				assert.ok(false);
+			}, () => {
+				assert.ok(true, 'yes, it was cancelled');
+			});
+
+			assert.ok(delayer.onSchedule());
+			delayer.unschedule();
+			assert.ok(!delayer.onSchedule());
+
+			return p;
+		});
+
+		test('cancel should cancel all calls to queue', function () {
+			let count = 0;
+			const factory = () => {
+				return Promise.resolve(++count);
+			};
+
+			const delayer = new Debouncer(0);
+			const promises: Promise<any>[] = [];
+
+			assert.ok(!delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then(undefined, () => { assert.ok(true, 'yes, it was cancelled'); }));
+			assert.ok(delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then(undefined, () => { assert.ok(true, 'yes, it was cancelled'); }));
+			assert.ok(delayer.onSchedule());
+
+			promises.push(delayer.queue(factory).then(undefined, () => { assert.ok(true, 'yes, it was cancelled'); }));
+			assert.ok(delayer.onSchedule());
+
+			delayer.unschedule();
+
+			return Promise.all(promises).then(() => {
+				assert.ok(!delayer.onSchedule());
+			});
+		});
+
+		test('queue, cancel, then queue again', function () {
+			let count = 0;
+			const factory = () => {
+				return Promise.resolve(++count);
+			};
+
+			const delayer = new Debouncer(0);
+			let promises: Promise<any>[] = [];
+
+			assert.ok(!delayer.onSchedule());
+
+			const p = delayer.queue(factory).then((result) => {
+				assert.strictEqual(result, 1);
+				assert.ok(!delayer.onSchedule());
+
+				promises.push(delayer.queue(factory).then(undefined, () => { assert.ok(true, 'yes, it was cancelled'); }));
+				assert.ok(delayer.onSchedule());
+
+				promises.push(delayer.queue(factory).then(undefined, () => { assert.ok(true, 'yes, it was cancelled'); }));
+				assert.ok(delayer.onSchedule());
+
+				delayer.unschedule();
+
+				const p = Promise.all(promises).then(() => {
+					promises = [];
+
+					assert.ok(!delayer.onSchedule());
+
+					promises.push(delayer.queue(factory).then(() => { assert.strictEqual(result, 1); assert.ok(!delayer.onSchedule()); }));
+					assert.ok(delayer.onSchedule());
+
+					promises.push(delayer.queue(factory).then(() => { assert.strictEqual(result, 1); assert.ok(!delayer.onSchedule()); }));
+					assert.ok(delayer.onSchedule());
+
+					const p = Promise.all(promises).then(() => {
+						assert.ok(!delayer.onSchedule());
+					});
+
+					assert.ok(delayer.onSchedule());
+
+					return p;
+				});
+
+				return p;
+			});
+
+			assert.ok(delayer.onSchedule());
+
+			return p;
+		});
+
+		test('last task should be the one getting called', function () {
+			const factoryFactory = (n: number) => () => {
+				return Promise.resolve(n);
+			};
+
+			const delayer = new Debouncer(0);
+			const promises: Promise<any>[] = [];
+
+			assert.ok(!delayer.onSchedule());
+
+			promises.push(delayer.queue(factoryFactory(1)).then((n) => { assert.strictEqual(n, 3); }));
+			promises.push(delayer.queue(factoryFactory(2)).then((n) => { assert.strictEqual(n, 3); }));
+			promises.push(delayer.queue(factoryFactory(3)).then((n) => { assert.strictEqual(n, 3); }));
+
+			const p = Promise.all(promises).then(() => {
+				assert.ok(!delayer.onSchedule());
+			});
+
+			assert.ok(delayer.onSchedule());
+
+			return p;
+		});
+	});
+
+    
 });
