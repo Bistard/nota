9e0ae854ec234a2e273b99fafef29b540fd67f01
SIHAN LI <1015813038@qq.com>
2022-09-05 00:10:21 -0400

    [Doc] `ReviverRegistrant`

--- src/code/platform/ipc/common/revive.ts
+++ src/code/platform/ipc/common/revive.ts
@@ -5,21 +5,45 @@ export const IReviverRegistrant = createRegistrant<IReviverRegistrant>(Registran
 
 export type PrototypeMatcher = (obj: Object) => boolean;
 
+/**
+ * An interface only for {@link ReviverRegistrant}.
+ */
 export interface IReviverRegistrant {
+    /**
+     * @description Register a prototype for future reviving process.
+     * @param prototype The prototype to be registered.
+     * @param matcher A matcher function to match if the given object mathces 
+     *                this registered prototype, if yes, the object will be 
+     *                recreated with the prototype.
+     */
     registerPrototype<Ctor extends Constructor<any>>(prototype: Ctor, matcher: PrototypeMatcher): void;
+    
+    /**
+     * @description Revives the given object with the correct prototype.
+     * @param obj The given object.
+     */
     revive<T>(obj: any): T;
 }
 
 /**
- * A reviver may be passed into `JSON.parse` so that the value computed by 
+ * @class A reviver may be passed into JSON.parse so that the value computed by 
  * parsing is transformed before being returned.
  * 
- * When processes are communicating between each other using IPC. After the 
- * deserilization process of `JSON.stringify` and `JSON.parse` the new created 
- * object will not inherit the original prototype which will cause unexpected 
- * behaviours during the usage.
+ * When processes are communicating with each other under IPC. After the 
+ * deserialization process of JSON.stringify and JSON.parse the new serialized 
+ * object will not inherit the original prototype which may cause unexpected 
+ * behaviours during the runtime.
  * 
- * To solve the above situation, // REVIEW
+ * To solve the above situation, you may register a prototype with a given 
+ * matcher to match the object when reviving. When invoking revive(obj) if the 
+ * given object is matched with any registered prototype then it will be 
+ * re-created with that prototype and returned.
+ * 
+ * @note
+ * When we are designing the matcher function, there is not really a perfect 
+ * approach to determine whether an object with a null prototype matches any 
+ * registered one. The way we do it is by checking whether the object has all 
+ * the required property names.
  */
 @IReviverRegistrant
 class ReviverRegistrant implements IReviverRegistrant {
@@ -61,5 +85,4 @@ class ReviverRegistrant implements IReviverRegistrant {
 
         return obj;
     }
-
 }
