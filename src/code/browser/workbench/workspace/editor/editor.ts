f62a0656827fd21285b7812c46a63e386bfb9d39
Bistard <lshh1015813038@gmail.com>
2022-11-15 19:48:31 -0500

    [Remove] milkdown editor code

--- src/code/browser/workbench/workspace/editor/editor.ts
+++ src/code/browser/workbench/workspace/editor/editor.ts
@@ -9,23 +9,6 @@ import { IInstantiationService } from "src/code/platform/instantiation/common/in
 import { registerSingleton } from "src/code/platform/instantiation/common/serviceCollection";
 import { EditorWidget, IEditorWidget } from "src/editor/editorWidget";
 import { EditorModel } from "src/editor/model/editorModel";
-import { Editor as MilkdownEditor, rootCtx } from '@milkdown/core';
-import { nordLight } from '@milkdown/theme-nord';
-import { commonmark } from '@milkdown/preset-commonmark';
-import { clipboard } from "@milkdown/plugin-clipboard";
-import { cursor } from "@milkdown/plugin-cursor";
-import { emoji } from "@milkdown/plugin-emoji";
-import { history } from "@milkdown/plugin-history";
-import { listener } from "@milkdown/plugin-listener";
-import { math } from "@milkdown/plugin-math";
-import { prism } from "@milkdown/plugin-prism";
-import { slash } from "@milkdown/plugin-slash";
-import { tooltip } from "@milkdown/plugin-tooltip";
-import { gfm } from "@milkdown/preset-gfm";
-import { menu } from '@milkdown/plugin-menu';
-import { block } from '@milkdown/plugin-block';
-import { upload } from "@milkdown/plugin-upload";
-import * as milkdownUtility from "@milkdown/utils";
 
 export const IEditorService = createService<IEditorService>('editor-service');
 
@@ -36,8 +19,6 @@ export interface IEditorService extends IComponent {
      * @param source The {@link URI} or an RUI in the string form.
      */
     openSource(source: URI | string): void;
-
-    updateMilkdownText(source: URI | string): Promise<void>;
 }
 
 export class Editor extends Component implements IEditorService {
@@ -56,8 +37,6 @@ export class Editor extends Component implements IEditorService {
     ) {
         super('editor', null, themeService, componentService);
         this._editorWidget = null;
-
-        this.createMilkdownEditor();
     }
 
     // [public methods]
@@ -79,49 +58,9 @@ export class Editor extends Component implements IEditorService {
                 console.warn(isSuccess);
                 return;
             }
-
+            
             this._editorWidget.attachModel(textModel);
         });
-    }
-
-    private editor!: MilkdownEditor;
-    public async createMilkdownEditor(): Promise<void> {
-        this.editor = 
-            await MilkdownEditor.make()
-            .config((ctx) => {
-                ctx.set(rootCtx, this.element.element);
-            })
-            .use(nordLight)
-            .use(commonmark)
-            .use(clipboard)
-            .use(cursor)
-            .use(emoji)
-            .use(history)
-            .use(listener)
-            .use(math)
-            .use(slash)
-            .use(tooltip)
-            .use(gfm)
-            .use(menu)
-            .use(block)
-            .use(prism)
-            .use(upload)
-            .create();
-    }
-
-    public async updateMilkdownText(source: URI | string): Promise<void> {
-        let uri: URI;
-        
-        if ((source instanceof URI)) {
-            uri = source;
-        } else {
-            uri = URI.fromFile(source);
-        }
-
-        const content = (await this.fileService.readFile(uri)).toString();
-
-        // replace all text
-        this.editor.action(milkdownUtility.replaceAll(content));
     }
 
     // [override protected methods]
