6de7abef4ccb86183bd402df98f8766e46e01c18
Bistard <lshh1015813038@gmail.com>
2022-11-17 23:44:40 -0500

    [Typo]

--- src/editor/common/prose.ts
+++ src/editor/common/prose.ts
@@ -1,6 +1,5 @@
-export { Node as ProseNode } from "prosemirror-model";
 export { EditorState as ProseEditorState } from "prosemirror-state";
-export { EditorView as ProseEditorView } from "prosemirror-view";
 export { Transaction } from "prosemirror-state";
+export { EditorView as ProseEditorView } from "prosemirror-view";
 export { Slice } from "prosemirror-model";
-export { MarkSpec, NodeSpec, Schema as ProseSchema } from "prosemirror-model";
+export { MarkSpec, NodeSpec, Node as ProseNode, NodeType as ProseNodeType, Attrs as ProseAttrs, Mark as ProseMark, Schema as ProseSchema } from "prosemirror-model";
