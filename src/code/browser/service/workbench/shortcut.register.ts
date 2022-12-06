b07c39c853ce9320af05c3561afe4cb52f5a990b
Bistard <lshh1015813038@gmail.com>
2022-12-06 15:41:24 -0500

    [Rename] workbenchContextKeys.ts

--- src/code/browser/service/workbench/shortcut.register.ts
+++ src/code/browser/service/workbench/shortcut.register.ts
@@ -1,7 +1,7 @@
 import { KeyCode, Shortcut } from "src/base/common/keyboard";
 import { IShortcutRegistrant, ShortcutWeight } from "src/code/browser/service/shortcut/shortcutRegistrant";
 import { WorkbenchCommand } from "src/code/browser/service/workbench/command.register";
-import { inDevelopContext } from "src/code/browser/service/workbench/context";
+import { WorkbenchContextKey } from "src/code/browser/service/workbench/workbenchContextKeys";
 import { REGISTRANTS } from "src/code/platform/registrant/common/registrant";
 
 export function workbenchShortcutRegistrations() {
@@ -11,20 +11,20 @@ export function workbenchShortcutRegistrations() {
         commandID: WorkbenchCommand.toggleDevTool,
         shortcut: new Shortcut(true, true, false, false, KeyCode.KeyI),
         weight: ShortcutWeight.Core,
-        when: inDevelopContext,
+        when: WorkbenchContextKey.inDevelopContext,
     });
     
     registrant.register({
         commandID: WorkbenchCommand.reloadWindow,
         shortcut: new Shortcut(true, false, false, false, KeyCode.KeyR),
         weight: ShortcutWeight.Core,
-        when: inDevelopContext,
+        when: WorkbenchContextKey.inDevelopContext,
     });
     
     registrant.register({
         commandID: WorkbenchCommand.closeApplication,
         shortcut: new Shortcut(true, false, false, false, KeyCode.KeyQ),
         weight: ShortcutWeight.Core,
-        when: inDevelopContext,
+        when: WorkbenchContextKey.inDevelopContext,
     });
 }
