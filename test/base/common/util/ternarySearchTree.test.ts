86346ce8dd97ffcbee5998109825ef80bf479150
SIHAN LI <lshh1015813038@gmail.com>
2023-02-20 12:34:32 -0500

    [Test] remove .dev.test.ts

--- test/base/common/util/ternarySearchTree.test.ts
+++ test/base/common/util/ternarySearchTree.test.ts
@@ -1,39 +1,12 @@
 import * as assert from 'assert';
-import { UserDefaultTypes } from 'electron';
-import { Random } from 'src/base/common/util/random';
-import { CreateTernarySearchTree, StringIterator, TernarySearchTree, TernarySearchTreeNode } from 'src/base/common/util/ternarySearchTree';
+import { URI } from 'src/base/common/file/uri';
+import { CreateTernarySearchTree, ITernarySearchTree, StringIterator, TernarySearchTree, TernarySearchTreeNode } from 'src/base/common/util/ternarySearchTree';
+import { isString } from 'src/base/common/util/type';
+
+type PossibleKey = string | URI;
 
 suite('ternarySearchTree-test', () => {
-    test('string-iterator', () => {
-        const iter = new StringIterator();
-        iter.reset('this');
-        
-        assert.strictEqual(iter.currItem(), 't');
-        assert.strictEqual(iter.hasNext(), true);
-        assert.strictEqual(iter.cmp('t'), 0);
-
-        assert.ok(iter.cmp('a') < 0);
-        assert.ok(iter.cmp('z') > 0);
-        assert.strictEqual(iter.cmp('t'), 0);
-
-        iter.next();
-        assert.strictEqual(iter.currItem(), 'h');
-        assert.strictEqual(iter.hasNext(), true);
-        
-        iter.next();
-        assert.strictEqual(iter.hasNext(), true);
-        iter.next();
-        assert.strictEqual(iter.currItem(), 's');
-        assert.strictEqual(iter.hasNext(), false);
-        
-        iter.next()
-        assert.strictEqual(iter.currItem(), undefined);
-
-        iter.reset('hello');
-        assert.strictEqual(iter.currItem(), 'h');
-        assert.strictEqual(iter.hasNext(), true);
-    });
-
+    
     function isbalanced(tree: TernarySearchTree<any, any>): boolean {
         const nodeBalanced = (node: TernarySearchTreeNode<any, any> | undefined): boolean => {
             if (!node) {
@@ -70,12 +43,12 @@ suite('ternarySearchTree-test', () => {
             map.set(key, value);
         }
 
-        // Test Get
+        // get
         map.forEach((value: E, key: string): void => {
             assert.strictEqual(value, tree.get(key));
         })
 
-        // Test foreach and iterator
+        // foreach and iterator
         let count = 0;
         tree.forEach((value: E, key: string): void => {
             assert.strictEqual(value, map.get(key));
@@ -91,6 +64,36 @@ suite('ternarySearchTree-test', () => {
 		}
 		assert.strictEqual(map.size, count);
     }
+
+    test('string-iterator', () => {
+        const iter = new StringIterator();
+        iter.reset('this');
+        
+        assert.strictEqual(iter.currItem(), 't');
+        assert.strictEqual(iter.hasNext(), true);
+        assert.strictEqual(iter.cmp('t'), 0);
+
+        assert.ok(iter.cmp('a') < 0);
+        assert.ok(iter.cmp('z') > 0);
+        assert.strictEqual(iter.cmp('t'), 0);
+
+        iter.next();
+        assert.strictEqual(iter.currItem(), 'h');
+        assert.strictEqual(iter.hasNext(), true);
+        
+        iter.next();
+        assert.strictEqual(iter.hasNext(), true);
+        iter.next();
+        assert.strictEqual(iter.currItem(), 's');
+        assert.strictEqual(iter.hasNext(), false);
+        
+        iter.next()
+        assert.strictEqual(iter.currItem(), undefined);
+
+        iter.reset('hello');
+        assert.strictEqual(iter.currItem(), 'h');
+        assert.strictEqual(iter.hasNext(), true);
+    });
 
     test('set & get', () => {
         let tree = CreateTernarySearchTree.forStrings<number>();
@@ -126,6 +129,7 @@ suite('ternarySearchTree-test', () => {
     });
 
     test('delete & cleanup', () => {
+
 		// normal delete
 		let tree = new TernarySearchTree<string, number>(new StringIterator());
 		tree.set('foo', 1);
@@ -156,15 +160,45 @@ suite('ternarySearchTree-test', () => {
 	});
 
     test('fill and clear', () => {
-        const tree = CreateTernarySearchTree.forStrings<number>();
-        const pairs: [string, number][] = [['foo', 0], ['bar', 1], ['bang', 2], ['bazz', 3]];
-        tree.fill(pairs);
 
-        for (const pair of pairs) {
-			assert.strictEqual(tree.get(pair[0]), pair[1]);
-		}
+        const fillAndClearGeneric = function(fillData: readonly [PossibleKey, number][]): void {
+            if (!fillData.length) {
+                return;
+            }
+            
+            let tree: ITernarySearchTree<PossibleKey, number>;
+            {
+                const firstData = fillData[0]![0]!;
+                if (URI.isURI(firstData)) {
+                    tree = CreateTernarySearchTree.forURIs<number, URI>();
+                } else if (isString(firstData)) {
+                    tree = CreateTernarySearchTree.forStrings<number, string>();
+                } else {
+                    throw new Error();
+                }
+            }
 
-        tree.clear();
-        assert.strictEqual(tree.getRoot(), undefined);
+            tree.fill(fillData);
+
+            // FIX: DELETE once it got fixed
+            for (const node of tree) {
+                console.log(node);
+            }
+
+            for (const pair of fillData) {
+                assert.strictEqual(tree.get(pair[0]), pair[1]);
+            }
+
+            tree.clear();
+            assert.strictEqual(tree.getRoot(), undefined);
+            // assert.strictEqual(tree.size(), 0); // TODO
+        };
+
+        // FIX
+        // string
+        fillAndClearGeneric([['foo', 0], ['bar', 1], ['bang', 2], ['bazz', 3]]);
+
+        // URI
+        // TODO
     });
 })
