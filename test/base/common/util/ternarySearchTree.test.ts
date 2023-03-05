643ea8ee0caa479e601c9d4234bf06a5ec97d767
SIHAN LI <lshh1015813038@gmail.com>
2023-03-05 01:43:43 -0500

    [Test] generic test on TST

--- test/base/common/util/ternarySearchTree.test.ts
+++ test/base/common/util/ternarySearchTree.test.ts
@@ -1,13 +1,13 @@
 import * as assert from 'assert';
 import { URI } from 'src/base/common/file/uri';
-import { CreateTernarySearchTree, ITernarySearchTree, StringIterator, TernarySearchTree, TernarySearchTreeNode } from 'src/base/common/util/ternarySearchTree';
+import { CreateTernarySearchTree, ITernarySearchTree, StringIterator, TernarySearchTree, TernarySearchTreeNode, UriIterator } from 'src/base/common/util/ternarySearchTree';
 import { isString } from 'src/base/common/util/type';
 
 type PossibleKey = string | URI;
 
 suite('ternarySearchTree-test', () => {
     
-    function isbalanced(tree: TernarySearchTree<any, any>): boolean {
+    function isbalanced(tree: ITernarySearchTree<any, any>): boolean {
         const nodeBalanced = (node: TernarySearchTreeNode<any, any> | undefined): boolean => {
             if (!node) {
                 return true;
@@ -22,7 +22,7 @@ suite('ternarySearchTree-test', () => {
     }
 
     // Note: items must be in order. 
-    function assertTstDfs<E>(tree: TernarySearchTree<string, E>, ...items: [string, E][]) {
+    function assertTstDfs<Key extends PossibleKey, E>(tree: ITernarySearchTree<Key, E>, ...items: [Key, E][]) {
         assert.ok(isbalanced(tree), 'TST is not balanced');
 
         let i = 0;
@@ -37,20 +37,20 @@ suite('ternarySearchTree-test', () => {
 
         assert.strictEqual(i, items.length);
 
-        const map = new Map<string, E>();
+        const map = new Map<Key, E>();
 
         for (const [key, value] of items) {
             map.set(key, value);
         }
 
         // get
-        map.forEach((value: E, key: string): void => {
+        map.forEach((value: E, key: Key): void => {
             assert.strictEqual(value, tree.get(key));
         })
 
         // foreach and iterator
         let count = 0;
-        tree.forEach((value: E, key: string): void => {
+        tree.forEach((value: E, key: Key): void => {
             assert.strictEqual(value, map.get(key));
             count++;
         });
@@ -95,89 +95,103 @@ suite('ternarySearchTree-test', () => {
         assert.strictEqual(iter.hasNext(), true);
     });
 
+    function generateTree(value: PossibleKey): ITernarySearchTree<PossibleKey, number> {
+        let tree: ITernarySearchTree<PossibleKey, number>;
+        if (URI.isURI(value)) {
+            tree = CreateTernarySearchTree.forURIs<number, URI>();
+        } else if (isString(value)) {
+            tree = CreateTernarySearchTree.forStrings<number, string>();
+        } else {
+            throw new Error();
+        }
+        return tree;
+    }
+
     test('set & get', () => {
-        let tree = CreateTernarySearchTree.forStrings<number>();
 
-        tree.set('foobar', 0);
-        tree.set('foobaz', 1);
-        assertTstDfs(tree, ['foobar', 0], ['foobaz', 1]); // same length
+        const testGeneric = function <Key extends PossibleKey>(foobar: Key, foobaz: Key, fooba: Key, foo: Key, bar: Key, foob: Key, bazz: Key): void {
+            let tree = generateTree(foo);
 
-		tree = CreateTernarySearchTree.forStrings<number>();
-		tree.set('foobar', 1);
-		tree.set('fooba', 2);
-		assertTstDfs(tree, ['fooba', 2], ['foobar', 1]); // shorter
+            tree.set(foobar, 0);
+            tree.set(foobaz, 1);
+            assertTstDfs(tree, [foobar, 0], [foobaz, 1]); // same length
 
-		tree = CreateTernarySearchTree.forStrings<number>();
-		tree.set('foo', 1);
-		tree.set('foo', 2);
-		assertTstDfs(tree, ['foo', 2]);
+            tree = generateTree(foo);
+            tree.set(foobar, 1);
+            tree.set(fooba, 2);
+            assertTstDfs(tree, [fooba, 2], [foobar, 1]); // shorter
 
-		tree = CreateTernarySearchTree.forStrings<number>();
-		tree.set('foo', 1);
-		tree.set('foobar', 2);
-		tree.set('bar', 3);
-		tree.set('foob', 4);
-		tree.set('bazz', 5);
+            tree = generateTree(foo);
+            tree.set(foo, 1);
+            tree.set(foo, 2);
+            assertTstDfs(tree, [foo, 2]);
 
-		assertTstDfs(tree,
-			['bar', 3],
-			['bazz', 5],
-			['foo', 1],
-			['foob', 4],
-			['foobar', 2],
-		);
+            tree = generateTree(foo);
+            tree.set(foo, 1);
+            tree.set(foobar, 2);
+            tree.set(bar, 3);
+            tree.set(foob, 4);
+            tree.set(bazz, 5);
+
+            assertTstDfs(tree,
+                [bar, 3],
+                [bazz, 5],
+                [foo, 1],
+                [foob, 4],
+                [foobar, 2],
+            );
+        };
+
+        testGeneric('foobar', 'foobaz', 'fooba', 'foo', 'bar', 'foob', 'bazz');
+        // FIX
+        testGeneric(URI.fromFile('foobar'), URI.fromFile('foobaz'), URI.fromFile('fooba'), URI.fromFile('foo'), URI.fromFile('bar'), URI.fromFile('foob'), URI.fromFile('bazz'));
     });
 
     test('delete & cleanup', () => {
 
-		// normal delete
-		let tree = new TernarySearchTree<string, number>(new StringIterator());
-		tree.set('foo', 1);
-		tree.set('foobar', 2);
-		tree.set('bar', 3);
-		assertTstDfs(tree, ['bar', 3], ['foo', 1], ['foobar', 2]);
-		tree.delete('foo');
-		assertTstDfs(tree, ['bar', 3], ['foobar', 2]);
-		tree.delete('foobar');
-		assertTstDfs(tree, ['bar', 3]);
+        const testGeneric = function <Key extends PossibleKey>(foo: Key, foobar: Key, bar: Key, foobarbaz: Key, fo: Key): void {
+            // normal delete
+            let tree = generateTree(foo);
+            tree.set(foo, 1);
+            tree.set(foobar, 2);
+            tree.set(bar, 3);
+            assertTstDfs(tree, [bar, 3], [foo, 1], [foobar, 2]);
+            tree.delete(foo);
+            assertTstDfs(tree, [bar, 3], [foobar, 2]);
+            tree.delete(foobar);
+            assertTstDfs(tree, [bar, 3]);
 
-		// superstr-delete
-		tree = new TernarySearchTree<string, number>(new StringIterator());
-		tree.set('foo', 1);
-		tree.set('foobar', 2);
-		tree.set('bar', 3);
-		tree.set('foobarbaz', 4);
-		tree.deleteSuperStr('foo');
-		assertTstDfs(tree, ['bar', 3], ['foo', 1]);
+            // superstr-delete
+            tree = generateTree(foo);
+            tree.set(foo, 1);
+            tree.set(foobar, 2);
+            tree.set(bar, 3);
+            tree.set(foobarbaz, 4);
+            tree.deleteSuperStr(foo);
+            assertTstDfs(tree, [bar, 3], [foo, 1]);
 
-		tree = new TernarySearchTree<string, number>(new StringIterator());
-		tree.set('foo', 1);
-		tree.set('foobar', 2);
-		tree.set('bar', 3);
-		tree.set('foobarbaz', 4);
-		tree.deleteSuperStr('fo');
-		assertTstDfs(tree, ['bar', 3]);
+            tree = generateTree(foo);
+            tree.set(foo, 1);
+            tree.set(foobar, 2);
+            tree.set(bar, 3);
+            tree.set(foobarbaz, 4);
+            tree.deleteSuperStr(fo);
+            assertTstDfs(tree, [bar, 3]);
+        };
+
+		testGeneric('foo', 'foobar', 'bar', 'foobarbaz', 'fo');
+        // FIX
+        testGeneric(URI.fromFile('foo'), URI.fromFile('foobar'), URI.fromFile('bar'), URI.fromFile('foobarbaz'), URI.fromFile('fo'));
 	});
 
     test('fill and clear', () => {
 
-        const fillAndClearGeneric = function(fillData: readonly [PossibleKey, number][]): void {
+        const fillAndClearGeneric = function (fillData: readonly [PossibleKey, number][]): void {
             if (!fillData.length) {
                 return;
             }
             
-            let tree: ITernarySearchTree<PossibleKey, number>;
-            {
-                const firstData = fillData[0]![0]!;
-                if (URI.isURI(firstData)) {
-                    tree = CreateTernarySearchTree.forURIs<number, URI>();
-                } else if (isString(firstData)) {
-                    tree = CreateTernarySearchTree.forStrings<number, string>();
-                } else {
-                    throw new Error();
-                }
-            }
-
+            let tree = generateTree(fillData[0]![0]!);
             tree.fill(fillData);
 
             for (const pair of fillData) {
@@ -186,12 +200,11 @@ suite('ternarySearchTree-test', () => {
 
             tree.clear();
             assert.strictEqual(tree.getRoot(), undefined);
-            // assert.strictEqual(tree.size(), 0); // TODO
+            assert.strictEqual(tree.size(), 0);
         };
-        // string
+        
         fillAndClearGeneric([['foo', 0], ['bar', 1], ['bang', 2], ['bazz', 3]]);
-
-        // URI
-        // TODO
+        // FIX
+        fillAndClearGeneric([[URI.fromFile('foo'), 0], [URI.fromFile('bar'), 1], [URI.fromFile('bang'), 2], [URI.fromFile('bazz'), 3]]);
     });
 })
