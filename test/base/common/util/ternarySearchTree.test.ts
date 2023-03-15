a5ca38505dee34aa172499e8b313fe194b58c3be
SIHAN LI <lshh1015813038@gmail.com>
2023-03-15 15:26:30 -0400

    [Test] testing CI/CD new script

--- test/base/common/util/ternarySearchTree.test.ts
+++ test/base/common/util/ternarySearchTree.test.ts
@@ -2,7 +2,7 @@ import * as assert from 'assert';
 import { URI } from 'src/base/common/file/uri';
 import { CreateTernarySearchTree, ITernarySearchTree, StringIterator, TernarySearchTree, TernarySearchTreeNode, UriIterator } from 'src/base/common/util/ternarySearchTree';
 
-suite('ternarySearchTree-test', () => {
+suite.only('ternarySearchTree-test', () => {
     
     function isbalanced(tree: ITernarySearchTree<any, any>): boolean {
         const nodeBalanced = (node: TernarySearchTreeNode<any, any> | undefined): boolean => {
