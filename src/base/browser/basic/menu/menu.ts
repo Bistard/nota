75f656a7888d5d07bd0236d70bf596391fd8e811
SIHAN LI <lshh1015813038@gmail.com>
2023-03-20 08:36:35 -0400

    [Add] trigger keys options

--- src/base/browser/basic/menu/menu.ts
+++ src/base/browser/basic/menu/menu.ts
@@ -4,7 +4,7 @@ import { AbstractMenuItem, IMenuAction, MenuAction, MenuItemType, MenuSeperatorI
 import { ActionList, IActionList } from "src/base/common/action";
 import { addDisposableListener, DomUtility, EventType } from "src/base/browser/basic/dom";
 import { Emitter, Register } from "src/base/common/event";
-import { createStandardKeyboardEvent, KeyCode } from "src/base/common/keyboard";
+import { createStandardKeyboardEvent, IStandardKeyboardEvent, KeyCode } from "src/base/common/keyboard";
 import { isNullable } from "src/base/common/util/type";
 
 /**
@@ -46,6 +46,13 @@ export interface IMenuOptions {
      * The current context (about the target) of the menu.
      */
     readonly context: unknown;
+
+    /**
+     * A list of possible trigger keys to determine which keys can execute the 
+     * current focused item.
+     * @default [KeyCode.Enter, KeyCode.Space]
+     */
+    readonly triggerKeys?: KeyCode[];
 }
 
 /**
@@ -54,8 +61,6 @@ export interface IMenuOptions {
  * has a functionality {@link IMenuAction}.
  * 
  * A {@link Menu} provides various types of item and can be found at {@link MenuItemType}.
- * 
- * // TODO: add focus functionality
  */
 export class Menu extends ActionList<AbstractMenuItem> implements IMenu {
 
@@ -67,6 +72,8 @@ export class Menu extends ActionList<AbstractMenuItem> implements IMenu {
 
     private readonly _focusTracker: FocusTracker;
     private _currFocusedIndex: number; // index
+
+    private readonly _triggerKeys: KeyCode[];
 
     private _submenu?: IMenu;
 
@@ -91,6 +98,7 @@ export class Menu extends ActionList<AbstractMenuItem> implements IMenu {
         
         this._submenu = undefined;
         this._currFocusedIndex = -1;
+        this._triggerKeys = opts.triggerKeys ?? [KeyCode.Enter, KeyCode.Space];
 
         this._focusTracker = this.__register(new FocusTracker(this._element, true));
         this.__registerListeners();
@@ -195,39 +203,38 @@ export class Menu extends ActionList<AbstractMenuItem> implements IMenu {
          */
         this.__register(addDisposableListener(this._element, EventType.keydown, (e) => {
             const event = createStandardKeyboardEvent(e);
+            let eventHandled = true;
 
             switch (event.key) {
                 case KeyCode.Escape: {
                     this._onDidClose.fire();
                     break;
                 }
-        
                 case KeyCode.Home: {
                     this.onFocus(0);
                     break;
                 }
-
                 case KeyCode.End: {
                     this.onFocus(this._items.length - 1);
                     break;
                 }
-
                 case KeyCode.UpArrow: {
                     this.__focusPrevious();
                     break;
                 }
-                
                 case KeyCode.DownArrow: {
                     this.__focusNext();
                     break;
                 }
-
                 default:
-                    // TODO: trigger item event
+                    eventHandled = false;
                     break;
             }
 
-
+            if (eventHandled) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
         }));
 
         /**
@@ -235,7 +242,13 @@ export class Menu extends ActionList<AbstractMenuItem> implements IMenu {
          */
         this.__register(addDisposableListener(this._element, EventType.keyup, (e) => {
             const event = createStandardKeyboardEvent(e);
-            // TODO
+            
+            // try to run the current focused item
+            if (this.__isTriggerKeys(event) && this.__hasAnyFocused()) {
+                this.run(this._currFocusedIndex);
+                event.preventDefault();
+                event.stopPropagation();
+            }
         }));
     }
 
@@ -252,7 +265,7 @@ export class Menu extends ActionList<AbstractMenuItem> implements IMenu {
             return;
         }
         
-        if (this._currFocusedIndex === -1) {
+        if (!this.__hasAnyFocused()) {
             this.__focusItemAt(0);
             return;
         }
@@ -281,6 +294,14 @@ export class Menu extends ActionList<AbstractMenuItem> implements IMenu {
 
         this._currFocusedIndex = newIndex;
         item.focus();
+    }
+
+    private __hasAnyFocused(): boolean {
+        return this._currFocusedIndex !== -1;
+    }
+
+    private __isTriggerKeys(event: IStandardKeyboardEvent): boolean {
+        return this._triggerKeys.findIndex(key => key === event.key) !== -1;
     }
     
     // [private helper methods - submenu]
