140e37b05608ef409c2d958e87b41ef0a6b15c13
SIHAN LI <1015813038@qq.com>
2023-03-22 11:34:36 -0400

    [Test] remove `suite.only`

--- test/base/browser/basic/menu.test.ts
+++ test/base/browser/basic/menu.test.ts
@@ -3,7 +3,7 @@ import { Menu, MenuWithSubmenu } from 'src/base/browser/basic/menu/menu';
 import { MenuSeperatorAction, SingleMenuAction, SubmenuAction } from 'src/base/browser/basic/menu/menuItem';
 import { KeyCode, Shortcut } from 'src/base/common/keyboard';
 
-suite.only('menu-test', () => {
+suite('menu-test', () => {
 
     test('menu', () => {
 
