8e6cff65f51a7c473c0f74a20491f8c52c6e67f2
SIHAN LI <lshh1015813038@gmail.com>
2023-07-03 03:33:08 -0400

    [Chore] style

--- scripts/webpack/webpack.config.base.js
+++ scripts/webpack/webpack.config.base.js
@@ -164,10 +164,13 @@ class WebpackBaseConfigurationProvider {
     checkNodeJsRequirement(minNodeVersion, currVersion) {
         const requiredNodeJsVersion = minNodeVersion.split('.');
         const currNodeJsVersion = currVersion.split('.');
+        
         for (let i = 0; i < currNodeJsVersion.length; i++) {
             if (Number(currNodeJsVersion[i]) > Number(requiredNodeJsVersion[i])) {
                 return;
-            } else if (Number(currNodeJsVersion[i]) < Number(requiredNodeJsVersion[i])) {
+            } 
+            
+            if (Number(currNodeJsVersion[i]) < Number(requiredNodeJsVersion[i])) {
                 const err = new Error(`Node.js version requires at least v${minNodeVersion}.`);
                 err.stack = undefined;
                 throw err;
