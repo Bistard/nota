9f4a923c498962dd3e0fb759399be4700d887bb0
SIHAN LI <lshh1015813038@gmail.com>
2023-07-19 00:39:47 -0400

    [Env] sort eslint output results by the presence of errors

--- scripts/linting/formatter.js
+++ scripts/linting/formatter.js
@@ -1,13 +1,13 @@
 module.exports = function (results) {
-
-    // Sort results by message type: errors first, then warnings
+    
+    // Sort results by the presence of errors (file with errors stay at the bottom)
     const sortedResults = results.sort((a, b) => {
-        if (a.errorCount > 0 && b.warningCount > 0) {
-            return -1;
+        if (a.errorCount > 0 && b.errorCount === 0) {
+            return 1;
         }
 
-        if (a.warningCount > 0 && b.errorCount > 0) {
-            return 1;
+        if (a.errorCount === 0 && b.errorCount > 0) {
+            return -1;
         }
 
         return 0;
@@ -20,16 +20,15 @@ module.exports = function (results) {
         const messages = result.messages;
 
         if (messages.length > 0) {
-            output += `\n${filePath}\n`;
+            output += `\n\u001b[4m${filePath}\u001b[0m\n`;
 
             messages.forEach(message => {
                 const { line, column, ruleId, message: description } = message;
                 const messageType = message.severity === 1 ? '\x1b[33mWarning\x1b[0m' : '\x1b[31mError\x1b[0m';
-                const config = `\x1b[2m${ruleId}\x1b[0m`;
-
-                output += `\x1b[2mLine ${line}, Column ${column}:\x1b[0m `;
+                
+                output += `\x1b[2mLine ${line}, Column ${column}\x1b[0m `;
                 output += `\x1b[1m${messageType}:\x1b[0m ${description} `;
-                output += `\x1b[2mConfiguration:\x1b[0m ${config}`;
+                output += `\x1b[2m${ruleId}\x1b[0m`;
                 output += '\n';
             });
         }
