3c5d2c7f58336f75a97006e1001ca5a7f8eb3f27
SIHAN LI <lshh1015813038@gmail.com>
2023-07-19 15:30:50 -0400

    [Feat] prettier formatter for eslint

--- scripts/linting/formatter.js
+++ scripts/linting/formatter.js
@@ -15,6 +15,11 @@ module.exports = function (results) {
 
     // generate output message
     let output = '';
+    let totalErrors = 0;
+    let totalWarnings = 0;
+    let totalFixableErrors = 0;
+    let totalFixableWarnings = 0;
+
     sortedResults.forEach(result => {
         const filePath = result.filePath;
         const messages = result.messages;
@@ -23,8 +28,20 @@ module.exports = function (results) {
             output += `\n\u001b[4m${filePath}\u001b[0m\n`;
 
             messages.forEach(message => {
-                const { line, column, ruleId, message: description } = message;
-                const messageType = message.severity === 1 ? '\x1b[33mWarning\x1b[0m' : '\x1b[31mError\x1b[0m';
+                const { line, column, ruleId, message: description, severity, fix } = message;
+                const messageType = severity === 1 ? '\x1b[33mWarning\x1b[0m' : '\x1b[31mError\x1b[0m';
+
+                if (severity === 1) {
+                    totalWarnings++;
+                    if (fix) {
+                        totalFixableWarnings++;
+                    }
+                } else if (severity === 2) {
+                    totalErrors++;
+                    if (fix) {
+                        totalFixableErrors++;
+                    }
+                }
 
                 const lineColumn = `${line}:${column}`;
                 const formattedMessage = `${messageType.padEnd(18)} ${description}`;
@@ -37,6 +54,9 @@ module.exports = function (results) {
             });
         }
     });
+
+    output += `\n\u001b[4m\u001b[1mTotal:\u001b[0m\u001b[0m \x1b[31m${totalErrors} errors\x1b[0m, \x1b[33m${totalWarnings} warnings\x1b[0m`;
+    output += `\n\u001b[4m\u001b[1mPotentially fixable with the '--fix' option:\u001b[0m\u001b[0m \x1b[32m${totalFixableErrors} errors\x1b[0m, \x1b[32m${totalFixableWarnings} warnings\x1b[0m\n`;
 
     return output;
 };
