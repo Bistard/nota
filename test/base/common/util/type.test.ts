ecf887261f0448c7dc685f1bfa09b4682fc33a25
SIHAN LI <lshh1015813038@gmail.com>
2023-07-20 18:23:19 -0400

    [Fix] remove `suite.only` should work just fine :)

--- test/base/common/util/type.test.ts
+++ test/base/common/util/type.test.ts
@@ -2,7 +2,7 @@ import * as assert from 'assert';
 import { LinkedList } from 'src/base/common/util/linkedList';
 import { AlphabetInString, AlphabetInStringCap, AlphabetInStringLow, AnyOf, AreEqual, CompareFn, ConcatArray, Constructor, DeepMutable, DeepReadonly, Dictionary, DightInString, IsArray, IsBoolean, IsNull, IsNumber, IsObject, IsString, IsTruthy, MapTypes, Mutable, Negate, NestedArray, NonUndefined, NulltoUndefined, NumberDictionary, Pair, Pop, Promisify, Push, Single, SplitString, StringDictionary, Triple, ifOrDefault, isBoolean, isEmptyObject, isIterable, isNonNullable, isNullable, isNumber, isObject, isPrimitive, isPromise } from 'src/base/common/util/type';
 
-suite.only('type-test', () => {
+suite('type-test', () => {
 
     test('isPrimitive', () => {
         assert.strictEqual(isPrimitive(0), true);
