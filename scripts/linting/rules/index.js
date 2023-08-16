896e5cfe264efcf71f6c9872083475fda146efca
SIHAN LI <lshh1015813038@gmail.com>
2023-08-16 10:40:33 -0400

    [Chore] linting script now parse ts files recursively

--- scripts/linting/rules/index.js
+++ scripts/linting/rules/index.js
@@ -2,10 +2,10 @@ const glob = require('glob');
 const path = require('path');
 require('ts-node').register({ experimentalResolver: true, transpileOnly: true });
 
-// Re-export all .ts files as rules
+// Re-export all `.ts` files as rules recursively
 const rules = {};
 glob
-.sync(`${__dirname}/*.ts`)
+.sync(`${__dirname}/**/*.ts`) // Adjusted the glob pattern to include subdirectories
 .forEach((file) => {
 	rules[path.basename(file, '.ts')] = require(file);
 });
