ad08ab432bd6ce59328de6fac258ada936fda532
SIHAN LI <lshh1015813038@gmail.com>
2023-08-21 16:20:27 -0400

    [Chore] style

--- src/platform/configuration/electron/mainConfigurationService.ts
+++ src/platform/configuration/electron/mainConfigurationService.ts
@@ -17,13 +17,10 @@ export class MainConfigurationService extends AbstractConfigurationService {
 
     // [public methods]
 
-    public set(section: Section, value: any): Promise<void>;
-    public set(section: Section, value: any, options: IConfigurationUpdateOptions): Promise<void>;
     public set(section: Section, value: any, options?: IConfigurationUpdateOptions): Promise<void> {
         return Promise.reject(new Error('[ConfigurationService] does not support `set`'));
     }
 
-    public delete(section: Section): Promise<void>;
     public delete(section: Section, options?: IConfigurationUpdateOptions): Promise<void> {
         return Promise.reject(new Error('[ConfigurationService] does not support `Delete`'));
     }
