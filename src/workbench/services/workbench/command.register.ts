35f68346d248fbae54e319001d6c3d668d50c3c9
SIHAN LI <1015813038@qq.com>
2023-09-01 16:18:15 -0400

    [Refactor] apply  `RegistrantService` in command systems

--- src/workbench/services/workbench/command.register.ts
+++ src/workbench/services/workbench/command.register.ts
@@ -1,9 +1,7 @@
-import { ICommandRegistrant } from "src/platform/command/common/commandRegistrant";
 import { IHostService } from "src/platform/host/common/hostService";
 import { ILifecycleService } from "src/platform/lifecycle/browser/browserLifecycleService";
-import { REGISTRANTS } from "src/platform/registrant/common/registrant";
-
-const registrant = REGISTRANTS.get(ICommandRegistrant);
+import { RegistrantType } from "src/platform/registrant/common/registrant";
+import { RegistrantService } from "src/platform/registrant/common/registrantService";
 
 export const enum WorkbenchCommands {
     toggleDevTool = 'toggle-develop-tool',
@@ -11,38 +9,41 @@ export const enum WorkbenchCommands {
     closeApplication = 'close-application',
 }
 
-export function workbenchCommandRegistrations(): void {
-
-    registrant.registerCommand(
-        {
-            id: WorkbenchCommands.toggleDevTool,
-            description: 'Toggle the developer tool of the whole application.',
-        },
-        (provider) => {
-            const hostService = provider.getOrCreateService(IHostService);
-            hostService.toggleDevTools();
-        },
-    );
-
-    registrant.registerCommand(
-        {
-            id: WorkbenchCommands.reloadWindow,
-            description: 'Reload the current window entirely.',
-        },
-        (provider) => {
-            const hostService = provider.getOrCreateService(IHostService);
-            hostService.reloadWebPage();
-        },
-    );
-
-    registrant.registerCommand(
-        {
-            id: WorkbenchCommands.closeApplication,
-            description: 'Close the application.',
-        },
-        (provider) => {
-            const lifecycleService = provider.getOrCreateService(ILifecycleService);
-            lifecycleService.quit();
-        },
-    );
-}
+export const rendererWorkbenchCommandRegister = RegistrantService.createRegister(
+    RegistrantType.Command, 
+    'rendererWorkbenchCommandRegister',
+    (registrant) => {
+        registrant.registerCommand(
+            {
+                id: WorkbenchCommands.toggleDevTool,
+                description: 'Toggle the developer tool of the whole application.',
+            },
+            (provider) => {
+                const hostService = provider.getOrCreateService(IHostService);
+                hostService.toggleDevTools();
+            },
+        );
+    
+        registrant.registerCommand(
+            {
+                id: WorkbenchCommands.reloadWindow,
+                description: 'Reload the current window entirely.',
+            },
+            (provider) => {
+                const hostService = provider.getOrCreateService(IHostService);
+                hostService.reloadWebPage();
+            },
+        );
+    
+        registrant.registerCommand(
+            {
+                id: WorkbenchCommands.closeApplication,
+                description: 'Close the application.',
+            },
+            (provider) => {
+                const lifecycleService = provider.getOrCreateService(ILifecycleService);
+                lifecycleService.quit();
+            },
+        );
+    },
+);
