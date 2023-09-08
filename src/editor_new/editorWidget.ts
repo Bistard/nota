eec6b69b696c68755502f924d52a2078d7fac0b8
SIHAN LI <lshh1015813038@gmail.com>
2023-09-08 18:36:10 -0400

    [Chore] create a new editor file for startup

---
+++ src/editor_new/editorWidget.ts
@@ -0,0 +1,22 @@
+import { Disposable } from "src/base/common/dispose";
+
+/**
+ * An interface only for {@link EditorWidget}.
+ */
+export interface IEditorWidget extends Disposable {
+
+}
+
+/**
+ * @class // TODO
+ */
+class EditorWidget extends Disposable implements IEditorWidget {
+
+    // [fields]
+
+    // [constructor]
+
+    // [public methods]
+
+    // [private helper methods]
+}
