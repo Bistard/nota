fea4f25e8b900578b0fc4770247a57421ab745c8
ARIA <2329152202@qq.com>
2023-12-24 20:43:32 -0500

    Update fileTreeService.ts

--- src/workbench/services/fileTree/fileTreeService.ts
+++ src/workbench/services/fileTree/fileTreeService.ts
@@ -12,9 +12,16 @@ import { FuzzyScore, IFilterOpts } from "src/base/common/fuzzy";
 import { FileItemFilter as FileItemFilter } from "src/workbench/services/fileTree/fileItemFilter";
 import { IConfigurationService } from "src/platform/configuration/common/configuration";
 import { SideViewConfiguration } from "src/workbench/parts/sideView/configuration.register";
+<<<<<<< Updated upstream:src/workbench/services/fileTree/fileTreeService.ts
 import { CompareFn } from "src/base/common/utilities/type";
 
 export interface IFileTreeService extends ITreeService<FileItem> {
+=======
+import * as fs from 'fs';
+import * as path from 'path';
+import { CompareFn } from "src/base/common/util/type";
+export interface IClassicTreeService extends ITreeService<ClassicItem> {
+>>>>>>> Stashed changes:src/workbench/services/classicTree/classicTreeService.ts
 
 }
 
@@ -33,7 +40,12 @@ export class FileTreeService extends Disposable implements IFileTreeService {
 
     // [field]
 
+<<<<<<< Updated upstream:src/workbench/services/fileTree/fileTreeService.ts
     private _tree?: IFileTree<FileItem, void>;
+=======
+    private _tree?: IClassicTree<ClassicItem, void>;
+    private customSortOrder: string[] = [];
+>>>>>>> Stashed changes:src/workbench/services/classicTree/classicTreeService.ts
 
     // [constructor]
 
@@ -43,6 +55,7 @@ export class FileTreeService extends Disposable implements IFileTreeService {
         @IFileService private readonly fileService: IFileService,
     ) {
         super();
+        this.loadCustomSortOrder();
     }
 
     // [getter]
@@ -110,6 +123,7 @@ export class FileTreeService extends Disposable implements IFileTreeService {
     public async close(): Promise<void> {
         // TODO
     }
+<<<<<<< Updated upstream:src/workbench/services/fileTree/fileTreeService.ts
 }
 
 // TODO: @AAsteria
@@ -122,13 +136,77 @@ class FileTreeCustomSorting extends Disposable {
 
     constructor() {
         super();
+=======
+
+    private loadCustomSortOrder(): void {
+        try {
+            const sortOrderPath = path.join(__dirname, 'sorting.json'); //不要json
+            if (fs.existsSync(sortOrderPath)) {
+                const data = fs.readFileSync(sortOrderPath, 'utf8');
+                this.customSortOrder = JSON.parse(data);
+            }
+        } catch (error) {
+            console.error('Error loading custom sort order:', error);
+            this.customSortOrder = [];
+        }
+    }
+}
+
+class FileTreeCustomSorting extends Disposable {
+
+    // [fields]
+    private customSortOrder: string[];
+
+    // [constructor]
+
+    constructor(customSortOrder: string[]) {
+        super();
+        this.customSortOrder = customSortOrder;
+>>>>>>> Stashed changes:src/workbench/services/classicTree/classicTreeService.ts
     }
 
     // [public methods]
 
+<<<<<<< Updated upstream:src/workbench/services/fileTree/fileTreeService.ts
     public compare(a: FileItem, b: FileItem): number {
+=======
+    public compare(a: ClassicItem, b: ClassicItem): number {
+>>>>>>> Stashed changes:src/workbench/services/classicTree/classicTreeService.ts
         return -1;
     }
 
     // [private helper methods]
+<<<<<<< Updated upstream:src/workbench/services/fileTree/fileTreeService.ts
+=======
+
+    private __buildFileSortingFunction(): CompareFn<ClassicItem> {
+        
+        // TODO: customzied FileItem sorting
+        // TODO: @AAsteria
+        // TODO: @duckSoup0203
+     
+        return (a: ClassicItem, b: ClassicItem): number => {
+            const customSortOrder = this.customSortOrder;
+            const indexA = customSortOrder.indexOf(a.name);
+            const indexB = customSortOrder.indexOf(b.name);
+
+            if (indexA !== -1 && indexB !== -1) {
+                return indexA - indexB;
+            } else if (indexA !== -1) {
+                return -1;
+            } else if (indexB !== -1) {
+                return 1;
+            } else {
+                // Default sorting logic
+                if (a.type === b.type) {
+                    return (a.name < b.name) ? -1 : 1;
+                } else if (a.isDirectory()) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        };
+    }
+>>>>>>> Stashed changes:src/workbench/services/classicTree/classicTreeService.ts
 }
