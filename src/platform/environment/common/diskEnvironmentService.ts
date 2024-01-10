9097d3fe2f5bdc2ef7c473880255116fc069b78d
SIHAN LI <lshh1015813038@gmail.com>
2024-01-10 17:36:31 -0500

    [Chore] better environment logging

--- src/platform/environment/common/diskEnvironmentService.ts
+++ src/platform/environment/common/diskEnvironmentService.ts
@@ -70,6 +70,6 @@ export class DiskEnvironmentService implements IDiskEnvironmentService {
     // [private helper methods]
 
     protected inspect(): void {
-        this.logService?.trace('DiskEnvironmentService', `Disk Environment loaded.`, { environment: getAllEnvironments(this) });
+        this.logService?.trace('DiskEnvironmentService', `Disk Environment loaded.`, getAllEnvironments(this));
     }
 }
