8c7c1b8a3098dc12c658c41f80401160cb9656ae
SIHAN LI <lshh1015813038@gmail.com>
2024-01-16 23:32:17 -0500

    [Chore]

--- src/editor/view/viewPart/editor/editorBase.ts
+++ src/editor/view/viewPart/editor/editorBase.ts
@@ -2,6 +2,7 @@ import { ProseEditorState, ProseEditorView } from "src/editor/common/proseMirror
 import { EditorExtensionType } from "src/editor/editorWidget";
 import { ViewContext } from "src/editor/view/editorView";
 import { EditorViewProxy, IEditorViewProxy } from "src/editor/view/viewPart/editor/adapter/editorViewProxy";
+import { EditorSchema } from "src/editor/viewModel/schema";
 
 export interface IEditorBase extends IEditorViewProxy {
     
@@ -23,12 +24,13 @@ export abstract class EditorBase extends EditorViewProxy implements IEditorBase 
     ) {
         // binding the view part of the extension to the proseMirror
         const viewExtensions = extensions.map(ext => ext.extension.getViewExtension());
+        const schema = context.viewModel.getSchema();
 
         super(
             new ProseEditorView(
                 container, 
                 {
-                    state: createtDefaultState(context),
+                    state: createtDefaultState(schema),
                     plugins: [
                         ...viewExtensions,
                     ],
@@ -48,9 +50,9 @@ export abstract class EditorBase extends EditorViewProxy implements IEditorBase 
 
 }
 
-function createtDefaultState(context: ViewContext): ProseEditorState {
+function createtDefaultState(schema: EditorSchema): ProseEditorState {
     return ProseEditorState.create({
-        schema: context.viewModel.getSchema(),
+        schema: schema,
         plugins: [],
     });
 }
