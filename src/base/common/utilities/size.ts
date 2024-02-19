cd6b037896218ee40610bb47e9c0f9a6380ea311
SIHAN LI <lshh1015813038@gmail.com>
2024-02-19 03:24:20 -0500

    [Refactor] `Size2D` base class

--- src/base/common/utilities/size.ts
+++ src/base/common/utilities/size.ts
@@ -4,47 +4,105 @@ export interface IMeasureable {
 }
 
 export interface IDimension {
-    readonly width: number;
-    readonly height: number;
+    width: number;
+    height: number;
 }
 
 export interface IPosition {
-	readonly top: number;
-	readonly left: number;
+	top: number;
+	left: number;
 }
 
-export interface IDomBox extends IDimension, IPosition {
-	// empty
+export interface ICoordinate {
+	x: number;
+	y: number;
 }
 
-export class Dimension implements IDimension {
+export interface IDomBox extends IDimension, IPosition {}
+
+interface ISize2D {
+	clone(a: number, b: number): this;
+	equals(other: this): boolean;
+}
+
+class Size2D {
+	
+	constructor(
+		protected _a: number,
+		protected _b: number,
+	) { }
+
+	public clone(a: number = this._a, b: number = this._b): this {
+		return new (<any>this.constructor)(a, b);
+	}
+
+	public equals(other: this): boolean {
+		if (other === this) {
+			return true;
+		}
+		return other._a === this._a && other._b === this._b;
+	}
+
+	public scale(factor: number): this {
+        return new (<any>this.constructor)(this._a * factor, this._b * factor);
+    }
+
+    public add(other: this): this {
+        return new (<any>this.constructor)(this._a + other._a, this._b + other._b);
+    }
+
+    public subtract(other: this): this {
+        return new (<any>this.constructor)(this._a - other._a, this._b - other._b);
+    }
+}
+
+export class Dimension extends Size2D implements IDimension, ISize2D {
 
 	public static readonly None = new Dimension(0, 0);
 
-	constructor(
-		public width: number,
-		public height: number,
-	) { }
-
-	public static create(other: IDimension): Dimension {
-		return new Dimension(other.width, other.height);
+	constructor(width: number, height: number) {
+		super(width, height);
 	}
 
-	public with(width: number = this.width, height: number = this.height): Dimension {
-		if (width !== this.width || height !== this.height) {
-			return new Dimension(width, height);
-		} else {
-			return this;
-		}
+	get width(): number {
+		return this._a;
+	}
+	
+	get height(): number {
+		return this._b;
+	}
+}
+
+export class Position extends Size2D implements IPosition, ISize2D {
+
+	public static readonly None = new Position(0, 0);
+
+	constructor(top: number, left: number) {
+		super(top, left);
 	}
 
-	public equals(a: Dimension | undefined, b: Dimension | undefined): boolean {
-		if (a === b) {
-			return true;
-		}
-		if (!a || !b) {
-			return false;
-		}
-		return a.width === b.width && a.height === b.height;
+	get top(): number {
+		return this._a;
+	}
+	
+	get left(): number {
+		return this._b;
+	}
+}
+
+export class Coordinate extends Size2D implements ICoordinate, ISize2D {
+
+	public static readonly None = new Coordinate(0, 0);
+
+	constructor(x: number, y: number) {
+		super(x, y);
+	}
+
+	get x(): number {
+		return this._a;
+	}
+	
+	get y(): number {
+		return this._b;
 	}
 }
