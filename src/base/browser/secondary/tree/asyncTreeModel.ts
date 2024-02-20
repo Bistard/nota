f693564aa39062011ebc64c25096c077ef809888
SIHAN LI <lshh1015813038@gmail.com>
2024-02-20 04:41:45 -0500

    [Refactor] `__reuseOrCreateNode`

--- src/base/browser/secondary/tree/asyncTreeModel.ts
+++ src/base/browser/secondary/tree/asyncTreeModel.ts
@@ -55,6 +55,13 @@ export class AsyncTreeModel<T, TFilter> extends FlexMultiTreeModel<T, TFilter> i
      */
     private readonly _nodeRefreshing = new Map<IAsyncNode<T, TFilter>, Promise<void>>();
 
+    private readonly __reuseOrCreateNode: (
+        childrenData: readonly T[],
+        parent: IAsyncNode<T, TFilter>,
+        newNodes: IAsyncNode<T, TFilter>[],
+        newNodesToRefresh: IAsyncNode<T, TFilter>[],
+    ) => void;
+
     // [constructor]
 
     constructor(
@@ -65,6 +72,12 @@ export class AsyncTreeModel<T, TFilter> extends FlexMultiTreeModel<T, TFilter> i
         super(rootData, view, opts);
         this._childrenProvider = opts.childrenProvider;
         this._identityProvider = opts.identityProvider;
+
+        if (this._identityProvider) {
+            this.__reuseOrCreateNode = (...args) => this.__tryReuseOldNode(this._identityProvider!, ...args);
+        } else {
+            this.__reuseOrCreateNode = (...args) => this.__alwaysCreateNewNode(...args);
+        }
     }
 
     // [public methods]
@@ -215,13 +228,8 @@ export class AsyncTreeModel<T, TFilter> extends FlexMultiTreeModel<T, TFilter> i
          * If the tree can identify the uniqueness of each node, then it is 
          * possible to reuse the old nodes instead of creating new ones.
          */
-        // TODO: preformance - avoid calling `if` all the time, we only need one `if` for just once.
-        if (this._identityProvider) {
-            this.__tryReuseOldNode(this._identityProvider, childrenData, node, childrenNodes, childrenNodesToRefresh);
-        } else {
-            this.__alwaysCreateNewNode(childrenData, node, childrenNodes, childrenNodesToRefresh);
-        }
-
+        this.__reuseOrCreateNode(childrenData, node, childrenNodes, childrenNodesToRefresh);
+        
         node.stale = true;
         node.oldChildren = node.children;
         node.children = childrenNodes;
