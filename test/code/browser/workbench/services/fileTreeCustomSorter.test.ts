5e236dd4dce8ccb41c6cc17530cfaa1023b516c1
SIHAN LI <lshh1015813038@gmail.com>
2024-02-24 19:25:50 -0500

    [Test] unit test for `updateMetadataLot` APIs

--- test/code/browser/workbench/services/fileTreeCustomSorter.test.ts
+++ test/code/browser/workbench/services/fileTreeCustomSorter.test.ts
@@ -5,7 +5,7 @@ import { FileType } from 'src/base/common/files/file';
 import { URI } from 'src/base/common/files/uri';
 import { Arrays } from 'src/base/common/utilities/array';
 import { generateMD5Hash } from 'src/base/common/utilities/hash';
-import { TreeLike } from 'src/base/common/utilities/type';
+import { Pair, TreeLike } from 'src/base/common/utilities/type';
 import { FileService } from 'src/platform/files/common/fileService';
 import { DiskFileSystemProvider } from 'src/platform/files/node/diskFileSystemProvider';
 import { FileItem } from 'src/workbench/services/fileTree/fileItem';
@@ -13,6 +13,7 @@ import { defaultFileItemCompareFn } from "src/workbench/services/fileTree/fileTr
 import { FileTreeCustomSorter, OrderChangeType } from 'src/workbench/services/fileTree/fileTreeCustomSorter';
 import { FileTreeNode, buildFileTree, findFileItemByPath, printFileStat } from 'test/utils/helpers';
 import { NullLogger, TestURI } from 'test/utils/testService';
+import { executeOnce } from 'src/base/common/utilities/function';
 
 suite('fileTreeCustomSorter-test', () => {
 
@@ -138,7 +139,7 @@ suite('fileTreeCustomSorter-test', () => {
         const expectOrders = order;
         
         // check the actual metadata are exactly same to the expected metadata
-        assert.ok(Arrays.exactEquals(actualOrders, expectOrders), `'${URI.toString(folder)}' orders are not exactly equal`);
+        assert.deepStrictEqual(actualOrders, expectOrders, `orders are not exactly equal at: '${URI.toString(folder)}'`);
     }
 
     async function updateMetadataToDiskManually(folder: URI, order: string[]): Promise<void> {
@@ -195,41 +196,60 @@ suite('fileTreeCustomSorter-test', () => {
         });
     });
 
+    /**
+     * @description It does the following thing in order:
+     *      1. build the FileItem based on the root.
+     *      2. run the provided `action` that modifies the root FileItem.
+     *      3. rebuild the FileItem based on the same root.
+     */
+    async function assertMetadataAction(opts: {
+        action: (root: FileItem) => Promise<void>;
+        assertFn: (newRoot: FileItem) => Promise<void>;
+    }): Promise<void> 
+    {
+        // before action is applied
+        const root = await buildFileItem(rootURI);
+        await opts.action(root);
+
+        // after action is applied
+        const newRoot = await buildFileItem(rootURI);
+        await opts.assertFn(newRoot);
+    }
+
+    function __getFileItemBy(root: FileItem, name: string): FileItem {
+        const defaultPathMapping = {
+            ['folder1']: [0],
+            ['folder2']: [1],
+            ['folder3']: [2],
+            ['file1']: [3],
+            ['file2']: [4],
+            ['file3']: [5],
+            ['folder1_file1']: [0, 0],
+            ['folder1_file2']: [0, 1],
+            ['folder1_file3']: [0, 2],
+        };
+        
+        const item = findFileItemByPath(root, defaultPathMapping[name]);
+        assert.ok(item);
+        assert.strictEqual(item.name, name);
+        return item;
+    }
+
     suite('Cache to Disk (updateMetadata)', () => {
 
         before(() => init());
         beforeEach(async () => refreshFileSystem());
         after(async () => cleanCache());
 
-        // assert function
-        async function assertUpdateAction(opts: {
-            action: (root: FileItem) => Promise<void>;
-            assertFn: (newRoot: FileItem) => Promise<void>;
-        }): Promise<void> 
-        {
-            // before action is applied
-            const root = await buildFileItem(rootURI);
-            await opts.action(root);
-
-            // after action is applied
-            const newRoot = await buildFileItem(rootURI);
-            await opts.assertFn(newRoot);
-        }
-
         test('Remove: Verify Metadata After Item Removal', async () => {
-            await assertUpdateAction({
+            await assertMetadataAction({
                 action: async root => {
-                    // 'folder1'
-                    const item = findFileItemByPath(root, [0])!;
-                    assert.strictEqual(item.name, 'folder1');
-
-                    // remove
+                    const item = __getFileItemBy(root, 'folder1');
                     await sorter.updateMetadata(OrderChangeType.Remove, item, 0).unwrap();
                 },
                 assertFn: async () => {
                     await assertMetadataInDisk(
-                        URI.join(rootURI, 'root'), 
-                        true,
+                        URI.join(rootURI, 'root'), true,
                         ['folder2', 'file1', 'file2', 'file3'],
                     );
                 },
@@ -237,19 +257,14 @@ suite('fileTreeCustomSorter-test', () => {
         });
 
         test('Swap: Confirm Metadata Reflects Swap Operation', async () => {
-            await assertUpdateAction({
+            await assertMetadataAction({
                 action: async root => {
-                    // 'folder1'
-                    const item = findFileItemByPath(root, [0])!;
-                    assert.strictEqual(item.name, 'folder1');
-
-                    // Swap
+                    const item = __getFileItemBy(root, 'folder1');
                     await sorter.updateMetadata(OrderChangeType.Swap, item, 0, 1).unwrap();
                 },
                 assertFn: async () => {
                     await assertMetadataInDisk(
-                        URI.join(rootURI, 'root'), 
-                        true,
+                        URI.join(rootURI, 'root'), true,
                         ['folder2', 'folder1', 'file1', 'file2', 'file3'],
                     );
                 },
@@ -257,7 +272,7 @@ suite('fileTreeCustomSorter-test', () => {
         });
         
         test('Add: Ensure Metadata After Item Addition', async () => {
-            await assertUpdateAction({
+            await assertMetadataAction({
                 action: async root => {
                     const uri = URI.join(rootURI, 'root', 'folder3');
 
@@ -271,7 +286,7 @@ suite('fileTreeCustomSorter-test', () => {
                         [],
                     );
 
-                    // Swap
+                    // Add
                     await sorter.updateMetadata(OrderChangeType.Add, folder3, 2).unwrap();
                 },
                 assertFn: async () => {
@@ -285,7 +300,7 @@ suite('fileTreeCustomSorter-test', () => {
         });
         
         test('Update: Check Metadata After Item Rename', async () => {
-            await assertUpdateAction({
+            await assertMetadataAction({
                 action: async root => {
                     const from = URI.join(rootURI, 'root', 'folder2');
                     const to = URI.join(rootURI, 'root', 'folder3');
@@ -308,7 +323,184 @@ suite('fileTreeCustomSorter-test', () => {
                 },
             });
         });
+    });
 
+    suite('Cache to Disk (updateMetadataLot)', () => {
 
+        before(async () => init());
+        beforeEach(async () => {
+            await refreshFileSystem();
+            // !!!
+            // MAKE SURE ONLY SAVE TO DISK ONCE, THIS IS MAINLY WHY IT SUPPORTS THIS API
+            // !!!
+            sorter['__saveMetadataIntoDisk'] = executeOnce(sorter['__saveMetadataIntoDisk']);
+        });
+        after(async () => cleanCache());
+
+        suite('removeLot', () => {
+            
+            // another wrapper
+            async function assertRemoveLotAction(itemIndicateLevel: string, deleteIdx: number[], resultMetadata: string[]): Promise<void> {
+                await assertMetadataAction({
+                    action: async root => {
+                        const item = __getFileItemBy(root, itemIndicateLevel);
+                        await sorter.updateMetadataLot(OrderChangeType.Remove, [item], deleteIdx).unwrap();
+                    },
+                    assertFn: async () => {
+                        await assertMetadataInDisk(
+                            URI.join(rootURI, 'root'), true,
+                            resultMetadata,
+                        );
+                    },
+                });
+            }
+
+            // remove 'folder1' and 'folder2'
+            test('Remove: remove two items in a sequence', async () => {
+                await assertRemoveLotAction('folder1', [0, 1], ['file1', 'file2', 'file3']);
+            });
+            
+            // remove 'folder1' and 'file1'
+            test('Remove: remove two items not in a sequence (case1)', async () => {
+                await assertRemoveLotAction('folder1', [0, 2], ['folder2', 'file2', 'file3']);
+            });
+            
+            // remove 'folder1' and 'file3'
+            test('Remove: remove two items not in a sequence (case2)', async () => {
+                await assertRemoveLotAction('folder1', [0, 4], ['folder2', 'file1', 'file2']);
+            });
+            
+            // remove 'folder1' 'file2' and 'file3'
+            test('Remove: remove two items not in a sequence (case3)', async () => {
+                await assertRemoveLotAction('folder1', [0, 3, 4], ['folder2', 'file1']);
+            });
+            
+            // remove all
+            test('Remove: remove all items (case4)', async () => {
+                await assertRemoveLotAction('folder1', Arrays.range(0, 5), []);
+            });
+        });
+
+        suite('addLot', () => {
+
+            /**
+             * Only support adding files to the root.
+             */
+            async function assertAddLotAction(toAddNames: string[], addIdx: number[], resultMetadata: string[]): Promise<void> {
+                await assertMetadataAction({
+                    action: async root => {
+                        const toAdd: FileItem[] = [];
+
+                        for (const name of toAddNames) {
+                            const fileURI = URI.join(root.uri, name);
+
+                            await fileService.createFile(fileURI, DataBuffer.alloc(0), { overwrite: true }).unwrap();
+                            const item = new FileItem(
+                                await fileService.stat(fileURI).unwrap(),
+                                root,
+                                [],
+                            );
+
+                            toAdd.push(item);
+                        }
+
+                        await sorter.updateMetadataLot(OrderChangeType.Add, toAdd, addIdx).unwrap();
+                    },
+                    assertFn: async () => {
+                        await assertMetadataInDisk(
+                            URI.join(rootURI, 'root'), true,
+                            resultMetadata,
+                        );
+                    },
+                });
+            }
+
+            test('Add: should support adding single item', async () => {
+                await assertAddLotAction(
+                    ['newFile1'], [0],
+                    ['newFile1', 'folder1', 'folder2', 'file1', 'file2', 'file3'],
+                );
+            });
+
+            test('Add: adding mutiple items (case1)', async () => {
+                await assertAddLotAction(
+                    ['newFile1', 'newFile2', 'newFile3'], [0, 1, 1],
+                    ['newFile1', 'folder1', 'newFile2', 'newFile3', 'folder2', 'file1', 'file2', 'file3'],
+                );
+            });
+            
+            test('Add: adding mutiple items (case2)', async () => {
+                await assertAddLotAction(
+                    ['newFile1', 'newFile2', 'newFile3', 'newFile4', 'newFile5'], [0, 1, 1, 3, 4],
+                    ['newFile1', 'folder1', 'newFile2', 'newFile3', 'folder2', 'file1', 'newFile4', 'file2', 'newFile5', 'file3'],
+                );
+            });
+            
+            test('Add: items and indice should have the same length', async () => {
+                await assert.rejects(() => assertAddLotAction(
+                    ['newFile1'], [0, 1, 1, 1, 1, 1, 1, 1, 1],
+                    ['newFile1', 'folder1', 'folder2', 'file1', 'file2', 'file3'],
+                ), 'items and indice should have the same length');
+            });
+        });
+
+        suite('updateLot', () => {
+
+            /**
+             * Only support updating files at the root.
+             */
+            async function assertUpdateLotAction(toUpdateNames: Pair<string, string>[], updateIdx: number[], resultMetadata: string[]): Promise<void> {
+                await assertMetadataAction({
+                    action: async root => {
+                        const toUpdate: FileItem[] = [];
+                        for (const [oldName, newName] of toUpdateNames) {
+                            const from = URI.join(root.uri, oldName);
+                            const to = URI.join(root.uri, newName);
+                            
+                            const toStat = await fileService.moveTo(from, to, true).unwrap();
+                            const newItem = new FileItem(toStat, root, []);
+
+                            toUpdate.push(newItem);
+                        }
+                        await sorter.updateMetadataLot(OrderChangeType.Update, toUpdate, updateIdx).unwrap();
+                    },
+                    assertFn: async () => {
+                        await assertMetadataInDisk(
+                            URI.join(rootURI, 'root'), true,
+                            resultMetadata,
+                        );
+                    },
+                });
+            }
+
+            test('Update: should support update single item', async () => {
+                await assertUpdateLotAction(
+                    [['folder1', 'folder3']], [0],
+                    ['folder3', 'folder2', 'file1', 'file2', 'file3']
+                );
+            });
+            
+            test('Update: update mutiple items', async () => {
+                await assertUpdateLotAction(
+                    [
+                        ['folder1', 'update_folder1'], 
+                        ['file2', 'update_file2'], 
+                        ['file3', 'update_file3']
+                    ], [0, 3, 4],
+                    ['update_folder1', 'folder2', 'file1', 'update_file2', 'update_file3']
+                );
+            });
+            
+            test('Update: items and indice should have the same length', async () => {
+                await assert.rejects(() => assertUpdateLotAction(
+                    [
+                        ['folder1', 'update_folder1'], 
+                        ['file2', 'update_file2'], 
+                        ['file3', 'update_file3']
+                    ], [1, 1, 1, 1, 1, 1, 1, 1],
+                    ['update_folder1', 'folder2', 'file1', 'update_file2', 'update_file3']
+                ), 'items and indice should have the same length');
+            });
+        });
     });
 });
