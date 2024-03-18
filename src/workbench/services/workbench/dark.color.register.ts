ed50c163f9ec3e8a50b22310eebe1790187009b4
SIHAN LI <lshh1015813038@gmail.com>
2024-03-17 21:13:13 -0400

    [Fix] invalid preset them ID registration

--- src/workbench/services/workbench/dark.color.register.ts
+++ src/workbench/services/workbench/dark.color.register.ts
@@ -1,14 +1,15 @@
 import { RegistrantType, createRegister } from "src/platform/registrant/common/registrant";
 import { THEME_COLORS } from "../theme/themeDefaults";
+import { PresetColorTheme } from "src/workbench/services/theme/theme";
 
 export const rendererDarkThemeColorRegister = createRegister(
     RegistrantType.Color,
     'rendererThemeColor',
     (registrant) => {
-        registrant.registerColor('dark', 'menu-item-focus-background-color', THEME_COLORS.onyx);
-        registrant.registerColor('dark', 'search-bar-background', THEME_COLORS.charcoal); 
-        registrant.registerColor('dark', 'selection-colour', THEME_COLORS.midnightBlue); 
-        registrant.registerColor('dark', 'toolbar-container-background', THEME_COLORS.ebony); 
-        registrant.registerColor('dark', 'light-menu-border-color', THEME_COLORS.outerSpace); 
+        registrant.registerColor(PresetColorTheme.DarkModern, 'menu-item-focus-background-color', THEME_COLORS.onyx);
+        registrant.registerColor(PresetColorTheme.DarkModern, 'search-bar-background', THEME_COLORS.charcoal); 
+        registrant.registerColor(PresetColorTheme.DarkModern, 'selection-colour', THEME_COLORS.midnightBlue); 
+        registrant.registerColor(PresetColorTheme.DarkModern, 'toolbar-container-background', THEME_COLORS.ebony); 
+        registrant.registerColor(PresetColorTheme.DarkModern, 'light-menu-border-color', THEME_COLORS.outerSpace); 
     },
 );
--- src/workbench/services/workbench/light.color.register.ts
+++ src/workbench/services/workbench/light.color.register.ts
@@ -1,14 +1,15 @@
 import { RegistrantType, createRegister } from "src/platform/registrant/common/registrant";
 import { THEME_COLORS } from "../theme/themeDefaults";
+import { PresetColorTheme } from "src/workbench/services/theme/theme";
 
 export const rendererLightThemeColorRegister = createRegister(
     RegistrantType.Color,
     'rendererThemeColor',
     (registrant) => {
-        registrant.registerColor('light', 'sidebar-background-color', THEME_COLORS.seaGreen);
-        registrant.registerColor('light', 'search-bar-background', THEME_COLORS.ghostWhite); 
-        registrant.registerColor('light', 'selection-colour', THEME_COLORS.lightSkyBlue); 
-        registrant.registerColor('light', 'toolbar-container-background', THEME_COLORS.ghostWhite); 
-        registrant.registerColor('light', 'light-menu-border-color', THEME_COLORS.gainsboro); 
+        registrant.registerColor(PresetColorTheme.LightModern, 'sidebar-background-color', THEME_COLORS.seaGreen);
+        registrant.registerColor(PresetColorTheme.LightModern, 'search-bar-background', THEME_COLORS.ghostWhite); 
+        registrant.registerColor(PresetColorTheme.LightModern, 'selection-colour', THEME_COLORS.lightSkyBlue); 
+        registrant.registerColor(PresetColorTheme.LightModern, 'toolbar-container-background', THEME_COLORS.ghostWhite); 
+        registrant.registerColor(PresetColorTheme.LightModern, 'light-menu-border-color', THEME_COLORS.gainsboro); 
     },
 );
