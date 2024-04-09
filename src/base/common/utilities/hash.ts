4a589a2dc03612166acdc57a66dbe413124fc1eb
SIHAN LI <lshh1015813038@gmail.com>
2024-04-09 17:52:09 -0400

    [Feat] remove `generateMD5Hash` API

--- src/base/common/utilities/hash.ts
+++ src/base/common/utilities/hash.ts
@@ -1,5 +1,3 @@
-import * as CryptoJS from 'crypto-js';
-
 export type HashNumber = number;
 export type HashString = string;
 
@@ -15,13 +13,4 @@ export function hash(s: string): number {
         h = (h << 5) - h + s.charCodeAt(--i) | 0;
     }
     return h; 
-}
-
-/**
- * @description Hashes a given string with MD5
- * @param input The given string.
- * @returns The hash code converted to a string
- */
-export function generateMD5Hash(input: string): string {
-    return CryptoJS.MD5(input).toString();
 }
