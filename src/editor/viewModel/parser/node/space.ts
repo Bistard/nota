984968904c1e4f98d59d559f63a856f9be596a34
SIHAN LI <lshh1015813038@gmail.com>
2024-05-23 05:25:49 -0400

    [Feat] `Space` is rendered as multiple empty paragraphs

--- src/editor/viewModel/parser/node/space.ts
+++ src/editor/viewModel/parser/node/space.ts
@@ -1,7 +1,7 @@
 import { TokenEnum } from "src/editor/common/markdown";
 import { EditorTokens } from "src/editor/common/model";
 import { ProseNodeSpec } from "src/editor/common/proseMirror";
-import { DocumentNode } from "src/editor/viewModel/parser/documentNode";
+import { DocumentNode, createDomOutputFromOptions } from "src/editor/viewModel/parser/documentNode";
 import { IDocumentParseState } from "src/editor/viewModel/parser/parser";
 
 /**
@@ -18,29 +18,26 @@ export class Space extends DocumentNode<EditorTokens.Space> {
         return {
             group: 'block',
             content: 'inline*',
+            parseDOM: [{ tag: 'p' }],
             toDOM: () => { 
-                return ['p', 0];
+                return createDomOutputFromOptions({
+                    type: 'nested',
+                    tagName: 'p',
+                    children: [0],
+                });
             }
         };
     }
 
     public parseFromToken(state: IDocumentParseState, token: EditorTokens.Space): void {
-        state.activateNode(this.ctor);
-
+        
         /**
-         * Calculates the whitespace string to be inserted into the document 
-         * for a space token.
-         * 
-         * This calculation involves two key adjustments:
-         * 1. Subtract 1 to offset the `repeat` method's addition of an extra 
-         *      character.
-         * 2. Subtract another 1 to account for the parser treating `\n\n` as a 
-         *      single space, ensuring the `spaces` string reflects the intended 
-         *      whitespace accurately.
+         * Iterate every new line, create a {@link Space} for it. Every space
+         * will be rendered as an empty paragraph.
          */
-        const spaces = '\n'.repeat(token.raw.length - 2);
-        state.addText(spaces);
-
-        state.deactivateNode();
+        for (let i = 0; i < token.raw.length; i++) {
+            state.activateNode(this.ctor);
+            state.deactivateNode();
+        }
     }
 }
