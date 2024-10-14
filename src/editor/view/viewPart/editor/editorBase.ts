8eab9c0b62d3bfb07f9292bd97cb08fc47ce71ba
SIHAN LI <lshh1015813038@gmail.com>
2024-10-14 19:58:46 -0400

    [Refactor] avoid passing `EditorExtension`s into `ProseEditorView` too soon

--- src/editor/view/viewPart/editor/editorBase.ts
+++ src/editor/view/viewPart/editor/editorBase.ts
@@ -1,5 +1,5 @@
 import 'src/editor/view/media/editor/editorBase.scss';
-import { ProseEditorView } from "src/editor/common/proseMirror";
+import { ProseEditorState, ProseEditorView } from "src/editor/common/proseMirror";
 import { EditorExtensionInfo } from "src/editor/editorWidget";
 import { ViewContext } from "src/editor/view/editorView";
 import { EditorViewProxy, IEditorViewProxy } from "src/editor/view/viewPart/editor/adapter/editorViewProxy";
@@ -26,21 +26,35 @@ export abstract class EditorBase extends EditorViewProxy implements IEditorBase 
 
         // binding the view part of the extension to the proseMirror
         const schema = context.viewModel.getSchema();
-
         const viewExtensionInfo = extensions.map(ext => { return { id: ext.id, extension: ext.extension.getViewExtension() }; });
-        const extensionArr = viewExtensionInfo.map(info => info.extension);
-
-        super(
-            context,
-            viewExtensionInfo,
-            new ProseEditorView(
-                container, 
-                {
-                    state: EditorViewProxy.__createNewViewStateFrom(schema, extensionArr, null),
-                }
-            ),
+        const view = new ProseEditorView(
+            container, 
+            {
+                state: ProseEditorState.create({
+                    schema: schema,
+                    doc: undefined,
+                    /**
+                     * Note:
+                     * 1. The {@link ProseEditorView} is always constructed 
+                     *    slightly before the {@link EditorModel.onDidBuild} 
+                     *    process completes. If we bind the extension immediately, 
+                     *    Initialization of {@link EditorExtension} will be 
+                     *    triggered at this point.
+                     * 2. When {@link EditorModel.onDidBuild} finishes, it will 
+                     *    call {@link this.render()} eventually, which will 
+                     *    cause the Initialization of {@link EditorExtension} to 
+                     *    be triggered again.
+                     * 
+                     * As a result, the Initialization of {@link EditorExtension} 
+                     * will be called twice in a short span of time. To avoid 
+                     * this, we only trigger them during the second step.
+                     */
+                    plugins: [],
+                }),
+            }
         );
 
+        super(context, viewExtensionInfo, view);
         this._container = container;
         this._context = context;
     }
