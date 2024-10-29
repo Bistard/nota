00adbffdad3f03d7a9f1d82926bb806c1ab1d79e
SIHAN LI <lshh1015813038@gmail.com>
2024-10-28 22:36:15 -0400

    [Env] we are using `jsdom` to polyfill the Web APIs when testing in Mocha, instead of `jsdom-global`

---
+++ scripts/tests/jsdomSetup.js
@@ -0,0 +1,47 @@
+/**
+ * This is a simple script to setup jsdom to polyfill Web APIs when testing in 
+ * Mocha (node.js) environment.
+ */
+
+const { JSDOM } = require('jsdom');
+
+// Create a JSDOM instance
+const { window } = new JSDOM('', {
+    url: "http://localhost",
+    pretendToBeVisual: true,
+});
+
+global.window = window;
+global.document = window.document;
+global.navigator = window.navigator;
+global.MutationObserver = window.MutationObserver;
+global.Range = window.Range;
+
+// Make properties of `window` available globally
+Object.keys(window).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+        global[property] = window[property];
+    }
+});
+
+window.console = global.console;
+global.navigator = {
+    userAgent: 'node.js',
+    platform: 'Node.js',
+};
+
+/**
+ * fix: Prevents the CodeMirror error 'getClientRects is undefined'
+ * @see https://github.com/jsdom/jsdom/issues/3002
+ */
+document.createRange = () => {
+    const range = new Range();
+    range.getClientRects = () => ({
+        item: () => null,
+        length: 0,
+        [Symbol.iterator]: function *() {
+            yield* [];
+        },
+    });
+    return range;
+};
