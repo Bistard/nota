c2a8e88fe5050b815fc16a2d7eb602618d82b160
SIHAN LI <lshh1015813038@gmail.com>
2024-11-02 01:30:44 -0400

    [Refactor] remove deprecated `GLOBAL` and use `globalThis`

--- src/platform/electron/browser/global.ts
+++ src/platform/electron/browser/global.ts
@@ -3,40 +3,17 @@ import type { Mutable } from "src/base/common/utilities/type";
 import type { ISandboxProcess } from "src/platform/electron/common/electronType";
 import type { IWindowConfiguration } from "src/platform/window/common/window";
 import { executeOnce } from "src/base/common/utilities/function";
-import { panic } from "src/base/common/utilities/panic";
 
 /**
  * Expose APIs from the main process at `preload.js`.
  * 
  * // REVIEW: perf - we may load all the files that renderer required at later time.
  * Since we are loading every files in the beginning of the application, thus
- * we cannot access the properties of `GLOBAL.nota` since `preload.js` have not
+ * we cannot access the properties of `globalThis.nota` since `preload.js` have not
  * been loaded and expose the APIs yet.
  * 
  * The trick to fix this is by using a init function.
  */
-
-/**
- * {@link window}: The object represents an open window in a browser. It may be
- * undefined once the code is not running within a browser, such as, via command
- * line script.
- * 
- * {@link global} Scripts running under Node.js have an object called global as 
- * their global object. It may be undefined once the Node.js is not accessible
- * in the current environment.
- */
-
-/**
- * An alias for {@link self} or {@link global} if window is undefined.
- */
-export const GLOBAL: any = (
-    typeof self === 'object' ?
-        self :
-        (typeof global === 'object' ?
-            global :
-            {}
-        )
-);
 
 export const ipcRenderer: IpcRenderer = <any>{};
 export const process: Mutable<ISandboxProcess> = <any>{};
@@ -47,11 +24,7 @@ export const WIN_CONFIGURATION: IWindowConfiguration = <any>{};
  * are exposed through the `preload.js`.
  */
 export const initExposedElectronAPIs = executeOnce(function () {
-    if (typeof GLOBAL === 'object') {
-        (<Mutable<IpcRenderer>>ipcRenderer) = GLOBAL.nota.ipcRenderer;
-        (<Mutable<ISandboxProcess>>process) = GLOBAL.nota.process;
-        (<Mutable<IWindowConfiguration>>WIN_CONFIGURATION) = GLOBAL.nota.WIN_CONFIGURATION;
-    } else {
-        panic('Cannot init exposed electron APIs');
-    }
+    (<Mutable<IpcRenderer>>ipcRenderer) = globalThis.nota.ipcRenderer;
+    (<Mutable<ISandboxProcess>>process) = globalThis.nota.process;
+    (<Mutable<IWindowConfiguration>>WIN_CONFIGURATION) = globalThis.nota.WIN_CONFIGURATION;
 });
