7d7e415579cc30837f5964f9a81dc49fc4b99e3d
SIHAN LI <lshh1015813038@gmail.com>
2024-11-03 22:53:39 -0500

    [Fix] typo

--- src/editor/contrib/builtInExtensionList.ts
+++ src/editor/contrib/builtInExtensionList.ts
@@ -3,7 +3,7 @@ import { EditorExtension } from "src/editor/common/editorExtension";
 import { EditorCommandExtension } from "src/editor/contrib/commandExtension/commandExtension";
 import { EditorAutoSaveExtension } from "src/editor/contrib/autoSaveExtension";
 import { EditorInputRuleExtension } from "src/editor/contrib/inputRuleExtension/inputRuleExtension";
-import { EditorHistoryExtension } from "src/editor/contrib/historyExtension/historyExtension";
+// import { EditorHistoryExtension } from "src/editor/contrib/historyExtension/historyExtension";
 
 export const enum EditorExtensionIDs {
     Command   = 'editor-command-extension',
