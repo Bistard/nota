15908259eb1c34a4e3d045fa8f6e8d36ed170511
AAsteria <2329152202@qq.com>
2024-11-13 23:11:48 -0800

    [Feat] menu initialization

--- src/platform/menu/electron/mainMenuService.ts
+++ src/platform/menu/electron/mainMenuService.ts
@@ -1,7 +1,7 @@
-import { app, BrowserWindow, ipcMain, Menu, MenuItem } from "electron";
+import { app, BrowserWindow, ipcMain, Menu, MenuItemConstructorOptions } from "electron";
 import { ILogService } from "src/base/common/logger";
 import { IS_MAC } from "src/base/common/platform";
-import { IMenuService, MenuTemplate } from "src/platform/menu/common/menuService";
+import { CommandID, IMenuService, MenuTemplate } from "src/platform/menu/common/menuService";
 
 export class MainMenuService implements IMenuService {
 
@@ -14,7 +14,7 @@ export class MainMenuService implements IMenuService {
     constructor(
         @ILogService private readonly logService: ILogService,
     ) { 
-
+        this.init();
     }
 
     // [public methods]
@@ -38,28 +38,42 @@ export class MainMenuService implements IMenuService {
         }
     }
 
-    private getMenuTemplate() {
+    private getMenuTemplate(): MenuItemConstructorOptions[] {
         return MenuTemplate.map((menuItem) => {
-            return {
-                ...menuItem,
-                submenu: menuItem.submenu?.map((subItem) => {
-                    if (subItem.commandId) {
-                        return {
-                            ...subItem,
-                            click: () => this.handleMenuClick(subItem.commandId),
-                        };
+            const { label, submenu } = menuItem;
+            const electronMenuItem: MenuItemConstructorOptions = { label };
+
+            if (submenu && Array.isArray(submenu)) {
+                electronMenuItem.submenu = submenu.map((subItem) => {
+                    const { label, role, type, commandId } = subItem;
+                    const menuItem: MenuItemConstructorOptions = {};
+
+                    if (role) {
+                        menuItem.role = role;
+                    } else if (type) {
+                        menuItem.type = type as MenuItemConstructorOptions['type'];
+                    } else if (label) {
+                        menuItem.label = label;
                     }
-                    return subItem;
-                }),
-            };
+
+                    if (commandId) {
+                        menuItem.click = () => this.handleMenuClick(commandId);
+                    }
+
+                    return menuItem;
+                });
+            }
+            return electronMenuItem;
         });
     }
 
-    private handleMenuClick(commandID: string) {
+    private handleMenuClick(commandID: CommandID) {
         const focusedWindow = BrowserWindow.getFocusedWindow();
         if (focusedWindow) {
             focusedWindow.webContents.send('menu-item-clicked', commandID);
             this.logService.info('MainMenuService', `MenuItem clicked - ${commandID}`);
+        } else {
+            this.logService.warn('MainMenuService', 'No focused window to send menu command');
         }
     }
 
