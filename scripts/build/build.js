0d41cff8eebbd6e9df2a19e7484961e1352dcbd3
AAsteria <2329152202@qq.com>
2024-12-06 23:01:59 -0800

    [Run] script files `i18n.js` and `nls.js`

--- scripts/build/build.js
+++ scripts/build/build.js
@@ -39,6 +39,46 @@ const { Times, Loggers, ScriptProcess, ScriptHelper } = require("../utility");
         process.exit(1);
     }
 
+    // output raw i18n locale JSON file
+    const i18n = new ScriptProcess(
+        'i18n',
+        'node',
+        ['scripts/build/i18n.js'],
+        [],
+        {
+            env: process.env,
+            cwd: cwd,
+            shell: true,
+            stdio: "inherit",
+        }
+    );
+
+    try {
+        await i18n.waiting();
+    } catch (err) {
+        process.exit(1);
+    }
+
+    // convert raw JSON locale file into 1D array and write into compiled file
+    const nls = new ScriptProcess(
+        'nls',
+        'node',
+        ['scripts/build/nls.js'],
+        [],
+        {
+            env: process.env,
+            cwd: cwd,
+            shell: true,
+            stdio: "inherit",
+        }
+    );
+
+    try {
+        await nls.waiting();
+    } catch (err) {
+        process.exit(1);
+    }
+
     // build with webpack
     const webpack = new ScriptProcess(
         'webpack',
