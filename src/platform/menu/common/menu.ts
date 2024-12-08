d1becbc8eca8ccd062abee70b890dcf995ef69d2
SIHAN LI <lshh1015813038@gmail.com>
2024-12-08 01:12:15 +0000

    [Refactor] remove deprecated type definitions

--- src/platform/menu/common/menu.ts
+++ src/platform/menu/common/menu.ts
@@ -4,28 +4,8 @@ import { createService, IService } from "src/platform/instantiation/common/decor
 
 export const IMenuService = createService<IMenuService>('menu-service');
 
-export interface IMenuService extends IService {}
-
-export const MenuLabels = {
-    File: 'File',
-    Edit: 'Edit',
-    View: 'View',
-    Help: 'Help'
-};
-
-export interface ISubMenuItem {
-    label?: string;
-    role?: Electron.MenuItemConstructorOptions['role'];
-    type?: Electron.MenuItemConstructorOptions['type'];
-
-    commandId?: string;
-    click?: () => void;
-    submenu?: ISubMenuItem[];
-}
-
-export interface IMenuItem {
-    label: string;
-    submenu?: ISubMenuItem[];
+export interface IMenuService extends IService {
+    // noop
 }
 
 /**
