59a73f342a9e22e5339acd0465b74b17813ee32e
SIHAN LI <lshh1015813038@gmail.com>
2024-12-11 21:06:01 -0500

    [Chore] unit test: assert the return value from `localize`

--- test/build/i18n/src/file2.ts
+++ test/build/i18n/src/file2.ts
@@ -1,8 +1,9 @@
+import * as assert from 'assert';
 import { II18nService } from "src/platform/i18n/browser/i18nService";
 
 export function file2(i18n: II18nService) {
-    i18n.localize('key6', 'value6');
-    i18n.localize('key6', 'value6');
-    i18n.localize('key6', 'value6');
-    i18n.localize('key7', 'value7');
+    assert.strictEqual('value6', i18n.localize('key6', 'value6'));
+    assert.strictEqual('value6', i18n.localize('key6', 'value6'));
+    assert.strictEqual('value6', i18n.localize('key6', 'value6'));
+    assert.strictEqual('value7', i18n.localize('key7', 'value7'));
 }
--- test/build/i18n/src/file3.ts
+++ test/build/i18n/src/file3.ts
@@ -1,9 +1,10 @@
+import * as assert from 'assert';
 import { II18nService } from "src/platform/i18n/browser/i18nService";
 
 export function file3(i18n: II18nService) {
-    i18n.localize('key8', 'value8');
-    i18n.localize('key9', 'value9');
-    i18n.localize('key10', 'value10');
-    i18n.localize('key11', '{num} between {str}', { num: 5, str: 'world' });
-    i18n.localize('key12', '{str1} between {str2}', { str1: 'hello', str2: 'world' });
+    assert.strictEqual('value8', i18n.localize('key8', 'value8'));
+    assert.strictEqual('value9', i18n.localize('key9', 'value9'));
+    assert.strictEqual('value10', i18n.localize('key10', 'value10'));
+    assert.strictEqual('5 between world', i18n.localize('key11', '{num} between {str}', { num: 5, str: 'world' }));
+    assert.strictEqual('hello between world', i18n.localize('key12', '{str1} between {str2}', { str1: 'hello', str2: 'world' }));
 }
