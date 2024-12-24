a8cf4019248e3e9fb20259a0f123b459090f19ff
SIHAN LI <lshh1015813038@gmail.com>
2024-12-23 23:30:17 -0500

    [Chore]

--- src/workbench/parts/workspace/workspaceService.ts
+++ src/workbench/parts/workspace/workspaceService.ts
@@ -1,11 +1,11 @@
 import { createService, IService } from 'src/platform/instantiation/common/decorator';
 import { IComponent } from 'src/workbench/services/component/component';
 
-
 export const IWorkspaceService = createService<IWorkspaceService>('workspace-service');
+
 /**
  * An interface only for {@link WorkspaceView}.
  */
-
 export interface IWorkspaceService extends IComponent, IService {
+
 }
