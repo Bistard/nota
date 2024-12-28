c06bf80445c05877078679018a34e34c5854604d
SIHAN LI <lshh1015813038@gmail.com>
2024-12-27 22:36:41 -0500

    [Chore]

--- src/platform/instantiation/common/descriptor.ts
+++ src/platform/instantiation/common/descriptor.ts
@@ -2,13 +2,9 @@ import { Constructor } from "src/base/common/utilities/type";
 import { InstantiationRequiredParameters } from "src/platform/instantiation/common/instantiation";
 
 export class ServiceDescriptor<T extends Constructor> {
-
 	constructor(
         public readonly ctor: T, 
         public readonly args: InstantiationRequiredParameters<T>, 
         public readonly supportsDelayedInstantiation: boolean = false
-    ) {
-        this.ctor = ctor;
-        this.supportsDelayedInstantiation = supportsDelayedInstantiation;
-    }
+    ) {}
 }
