d7e51a1a16b68a437335798956b6140371023370
SIHAN LI <1015813038@qq.com>
2025-02-08 14:44:12 -0500

    [Test] a bit more tests on `ProseTools`

--- test/editor/view/proseUtility.test.ts
+++ test/editor/view/proseUtility.test.ts
@@ -2,7 +2,7 @@ import * as assert from 'assert';
 import { ProseTools } from 'src/editor/common/proseUtility';
 import { ProseUtilsTest } from 'test/editor/view/editorHelpers';
 
-const { doc, p, image, blockquote } = ProseUtilsTest.defaultNodes;
+const { doc, p, image, blockquote, text } = ProseUtilsTest.defaultNodes;
 
 suite('proseUtility-test', () => {
 
@@ -47,6 +47,50 @@ suite('proseUtility-test', () => {
         assert.strictEqual(ProseTools.Node.getNodeSize(node), 5); // size of 'p'
 
         view.destroy();
+    });
+
+    test('isTextBlock for paragraph and blockquote', () => {
+        const { state, view } = ProseUtilsTest.buildEditor(doc(p('Text'), blockquote(p())));
+        const paraNode = ProseTools.Position.getNodeAt(state, 1);
+        const quoteNode = ProseTools.Position.getNodeAt(state, 6);
+        
+        assert.strictEqual(ProseTools.Node.isTextBlock(paraNode), true);
+        assert.strictEqual(ProseTools.Node.isTextBlock(quoteNode), false);
+        view.destroy();
+    });
+
+    suite('Cursor namespace', () => {
+        test('isCursor detects empty selection', () => {
+            const { state, view } = ProseUtilsTest.buildEditor(doc(p('Text')));
+            assert.strictEqual(ProseTools.Cursor.isCursor(state.selection), true);
+            view.destroy();
+        });
+    
+        test('isOnEmpty detects empty textblock', () => {
+            const { state, view } = ProseUtilsTest.buildEditor(doc(p()));
+            const cursor = ProseTools.Cursor.getPosition(state.selection as any);
+            assert.strictEqual(ProseTools.Cursor.isOnEmpty(state.selection as any), true);
+            view.destroy();
+        });
+    });
+
+    suite('Text namespace', () => {
+        test('getWordBound detects word boundaries', () => {
+            /**
+             * 0   1 2 3 4 5 6    7
+             *  <p> H e l l o </p>
+             */
+            const { state, view } = ProseUtilsTest.buildEditor(doc(p('Hello')));
+            const pos = ProseTools.Position.resolve(state, 3); // 在字母'l'处
+            const bound = ProseTools.Text.getWordBound(pos);
+            
+            assert.deepStrictEqual(bound, { from: 1, to: 6 });
+            view.destroy();
+        });
+    });
+
+    suite('Position namespace', () => {
+        // todo
     });
 });
 
