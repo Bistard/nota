8da07f9d10f3f5234d98bdb02ebb5761f28427e3
SIHAN LI <1015813038@qq.com>
2025-02-10 19:14:27 -0500

    [Feat] localization support for markdown tokens in English and Chinese

--- src/editor/common/markdown.ts
+++ src/editor/common/markdown.ts
@@ -1,3 +1,5 @@
+import type { II18nService } from "src/platform/i18n/browser/i18nService";
+
 /**
  * Those token types are identical to {@link marked.Tokens.type}.
  */
@@ -29,4 +31,27 @@ export const enum MarkEnum {
     Codespan = 'codespan',
     // marked.Tokens.Tag // TODO
     Del = 'del', // TODO
+}
+
+let TOKEN_READABLE_NAMES: Record<string, string> | undefined = undefined;
+export function getTokenReadableName(i18n: II18nService, token: string | TokenEnum): string {
+    if (TOKEN_READABLE_NAMES) {
+        return TOKEN_READABLE_NAMES[token] ?? 'UNKNOWN';
+    }
+
+    TOKEN_READABLE_NAMES = {
+        [TokenEnum.Space]: i18n.localize('space', 'Space'),
+        [TokenEnum.CodeBlock]: i18n.localize('code', 'Code'),
+        [TokenEnum.Heading]: i18n.localize('heading', 'Heading'),
+        [TokenEnum.Table]: i18n.localize('table', 'Table'),
+        [TokenEnum.HorizontalRule]: i18n.localize('horizontalRule', 'Divider'),
+        [TokenEnum.Blockquote]: i18n.localize('blockquote', 'Quote'),
+        [TokenEnum.List]: i18n.localize('list', 'List'),
+        [TokenEnum.Paragraph]: i18n.localize('paragraph', 'Paragraph'),
+        [TokenEnum.HTML]: i18n.localize('html', 'HTML'),
+        [TokenEnum.Image]: i18n.localize('image', 'Image'),
+        [TokenEnum.MathBlock]: i18n.localize('mathBlock', 'Math'),
+    };
+
+    return TOKEN_READABLE_NAMES[token] ?? 'UNKNOWN';
 }
